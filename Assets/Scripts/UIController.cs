33d34a14ba5c75168a0982755ef6b5dc809a8cf3
Kazuki Matsumoto <karasusan@gmail.com>
2019-06-02 21:30:50 +0900

    mouseevent sample

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] RectTransform cursor;
+
+    void FixedUpdate()
+    {
+        var delta = Mouse.current.delta.ReadValue();
+        cursor.Translate(delta.x, -delta.y, 0);
+    }
+}
