4379059a427a0247adc56c354786d5060da040b4
Kazuki Matsumoto <karasusan@gmail.com>
2019-06-03 00:07:34 +0900

    Added touch event

--- Assets/Scripts/RemoteInput.cs
+++ Assets/Scripts/RemoteInput.cs
@@ -14,17 +14,33 @@ namespace Unity.RenderStreaming
     enum EventType
     {
         Keyboard = 0,
+        MouseDown = 2,
         MouseMove = 4,
+        MouseWheel = 5,
+        TouchMove = 6
     }
 
     public static class RemoteInput
     {
         static Keyboard keyboard;
         static Mouse mouse;
+        static Touchscreen touch;
+
+        static TDevice GetOrAddDevice<TDevice>() where TDevice : InputDevice
+        {
+            var device = InputSystem.GetDevice<TDevice>();
+            if(device != null)
+            {
+                return device;
+            }
+            return InputSystem.AddDevice<TDevice>();
+        }
+
         static RemoteInput()
         {
-            keyboard = InputSystem.GetDevice<Keyboard>();
-            mouse = InputSystem.GetDevice<Mouse>();
+            keyboard = GetOrAddDevice<Keyboard>();
+            mouse = GetOrAddDevice<Mouse>();
+            touch = GetOrAddDevice<Touchscreen>();
         }
 
         public static void ProcessInput(byte[] bytes)
@@ -34,11 +50,20 @@ namespace Unity.RenderStreaming
                 case EventType.Keyboard:
                     ProcessKeyEvent((char)bytes[1]);
                     break;
+                case EventType.MouseDown:
+                    ProcessMouseDownEvent(bytes[1]);
+                    break;
                 case EventType.MouseMove:
                     var deltaX = BitConverter.ToInt16(bytes, 1);
                     var deltaY = BitConverter.ToInt16(bytes, 3);
-                    ProcessMouseMoveEvent(deltaX, deltaY);
+                    ProcessMouseMoveEvent(deltaX, deltaY, bytes[5]);
                     break;
+                case EventType.TouchMove:
+                    var pageX = BitConverter.ToInt16(bytes, 1);
+                    var pageY = BitConverter.ToInt16(bytes, 3);
+                    ProcessTouchMoveEvent(pageX, pageY);
+                    break;
+
             }
         }
 
@@ -49,9 +74,21 @@ namespace Unity.RenderStreaming
             InputSystem.Update();
         }
 
-        static void ProcessMouseMoveEvent(short deltaX, short deltaY)
+        static void ProcessMouseMoveEvent(short deltaX, short deltaY, byte button)
         {
-            InputSystem.QueueStateEvent(mouse, new MouseState { delta = new Vector2Int(deltaX, deltaY)});
+            InputSystem.QueueStateEvent(mouse, new MouseState { delta = new Vector2Int(deltaX, deltaY), buttons = button });
+            InputSystem.Update();
+        }
+
+        static void ProcessMouseDownEvent(byte button)
+        {
+            InputSystem.QueueStateEvent(mouse, new MouseState { buttons = button });
+            InputSystem.Update();
+        }
+
+        static void ProcessTouchMoveEvent(short pageX, short pageY)
+        {
+            InputSystem.QueueStateEvent(touch, new TouchState { position = new Vector2Int(pageX, pageY) });
             InputSystem.Update();
         }
     }
