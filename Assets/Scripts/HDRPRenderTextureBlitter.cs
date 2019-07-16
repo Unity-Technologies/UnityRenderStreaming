e172783bcfb7e32551ba7bddcca1b3c8d86300a0
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-07-16 11:38:26 +0900

    Blits the render texture in Unity, so that the final rendered result is not black

---
+++ Assets/Scripts/HDRPRenderTextureBlitter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering;            //RenderPipeline
+using UnityEngine.Experimental.Rendering.HDPipeline; //HDAdditionalCameraData
+
+//Blits the render texture of a camera into the screen.
+//Uses RenderPipeline.beginCameraRendering instead of OnPostRender() since OnPostRender() is being reworked on HDRP
+[RequireComponent(typeof(Camera)), RequireComponent(typeof(HDAdditionalCameraData))]
+public class HDRPRenderTextureBlitter : MonoBehaviour
+{
+    [SerializeField] Camera m_rtCamera;
+
+    Camera                  m_cam;
+    HDAdditionalCameraData  m_hdData;
+
+    private void OnEnable() {
+        m_cam = GetComponent<Camera>();
+        m_hdData = GetComponent<HDAdditionalCameraData>();
+
+        //Render nothing 
+        m_cam.clearFlags = CameraClearFlags.Nothing;
+        m_cam.cullingMask = 0; 
+        m_hdData.fullscreenPassthrough = true;
+
+
+        RenderPipeline.beginCameraRendering += OnBeginCameraRendering;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    private void OnDisable() {
+        RenderPipeline.beginCameraRendering -= OnBeginCameraRendering;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void OnBeginCameraRendering(Camera cam) {
+        if (cam == m_cam && null != m_rtCamera.targetTexture ) {
+            Graphics.Blit(m_rtCamera.targetTexture, (RenderTexture) null);
+        }
+    }
+
+}
