87983d798107fa6fffd0e0def9cb063eeabd7e5e
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2019-07-24 19:12:19 +0900

    fix: Changed inspector parameter to use TURN server (#84)
    * Changed inspector parameter to use TURN server
    
    * Changed layout of the RenderStreaming inspector

---
+++ Assets/Scripts/Editor/RenderStreamingEditor.cs
@@ -0,0 +1,45 @@
+﻿using Unity.RenderStreaming;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RenderStreaming))]
+public class RenderStreamingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("urlSignaling"));
+            ShowIceServerList(serializedObject.FindProperty("iceServers"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("interval"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("captureCamera"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("arrayButtonClickEvent"), true);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    static void ShowIceServerList(SerializedProperty list)
+    {
+        EditorGUILayout.PropertyField(list.FindPropertyRelative("Array.size"), new GUIContent(list.displayName));
+        if (list.isExpanded)
+        {
+            EditorGUI.indentLevel += 1;
+            for (int i = 0; i < list.arraySize; i++)
+            {
+                var element = list.GetArrayElementAtIndex(i);
+                var label = "Ice server [" + i + "]";
+                EditorGUILayout.PropertyField(element, new GUIContent(label));
+                if (element.isExpanded)
+                {
+                    EditorGUI.indentLevel += 1;
+                    EditorGUILayout.PropertyField(element.FindPropertyRelative("urls"), true);
+                    EditorGUILayout.PropertyField(element.FindPropertyRelative("username"));
+                    EditorGUILayout.PropertyField(element.FindPropertyRelative("credential"));
+                    EditorGUILayout.PropertyField(element.FindPropertyRelative("credentialType"));
+                    EditorGUI.indentLevel -= 1;
+                }
+            }
+            EditorGUI.indentLevel -= 1;
+        }
+    }
+}
