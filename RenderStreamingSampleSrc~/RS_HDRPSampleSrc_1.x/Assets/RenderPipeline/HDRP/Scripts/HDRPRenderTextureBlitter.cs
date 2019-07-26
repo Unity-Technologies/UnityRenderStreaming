2472f5d511b70110e715d8a9e426b97000bf3244
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-07-26 17:04:17 +0900

    Copied HDRPRenderTextureBlitter into another script under RS_HDRPSampleSrc
    This script should be part of the UnityPackage file, since it depends on HDRP

---
+++ RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/RenderPipeline/HDRP/Scripts/HDRPRenderTextureBlitter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+#if UNITY_2019_1 || UNITY_2019_2 //HDRP 5.x, 6.x
+using UnityEngine.Experimental.Rendering.HDPipeline; 
+#else //HDRP 7.x and above
+using UnityEngine.Rendering.HighDefinition;
+#endif
+
+//Blits the render texture of a camera into the screen.
+//Uses RenderPipelineManager.beginCameraRendering instead of OnPostRender() since OnPostRender() is being reworked on HDRP
+[RequireComponent(typeof(Camera)), RequireComponent(typeof(HDAdditionalCameraData))]
+public class HDRPRenderTextureBlitter : MonoBehaviour
+{
+    [SerializeField] Camera m_rtCamera = null;
+
+    Camera                  m_cam;
+    HDAdditionalCameraData  m_hdData;
+
+    private void OnEnable() {
+        m_cam = GetComponent<Camera>();
+        m_hdData = GetComponent<HDAdditionalCameraData>();
+
+        //Render nothing 
+        m_cam.clearFlags = CameraClearFlags.Nothing;
+        m_cam.cullingMask = 0; 
+        m_hdData.fullscreenPassthrough = true;
+
+
+        UnityEngine.Rendering.RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    private void OnDisable() {
+        UnityEngine.Rendering.RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void OnBeginCameraRendering(UnityEngine.Rendering.ScriptableRenderContext context, Camera cam) {
+        if (cam == m_cam && null != m_rtCamera.targetTexture ) {
+            Graphics.Blit(m_rtCamera.targetTexture, (RenderTexture) null);
+        }
+    }
+
+}
