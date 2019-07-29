bfafcbf21968fcac550cfe39f457d0bfe47b4124
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-07-29 14:44:28 +0900

    refactor: added a Unity project to create the HDRP sample unitypackage and restructured the main Unity project (#90)
    * Add a Unity project to create a simple  HDRP sample of RenderStreaming (unitypackage)
    
    * Add a utility class for adding HDRP package and importing the HDRP sample of RenderStreaming
    
    * Removing unused assets
    
    * Copying Settings to RenderPipeline/Settings and moving HDRPDefaultResources to RenderPipeline/HDRP
    
    Makes it explicit, which one is settings (CoreRP) and which one is HDRP
    
    * Reconfigured the simple scene using the arranged HDRP and core RP assets.
    
    Also delete Settings symlink
    
    * Copied HDRPRenderTextureBlitter into another script under RS_HDRPSampleSrc
    
    This script should be part of the UnityPackage file, since it depends on HDRP
    
    * Updated simple Scene and project settings
    
    * Updated com.unity.renderstreaming
    - Removed HDRP dependency from com.unity.renderstreaming
    - Add a UnityPackage that depends on HDRP
    
    * Change the path of the sample of com.unity.renderstreaming
    
    * Rearranging the render pipeline assets according to com.unity.renderstreaming
    
    * Update samplescene after arranging the assets
    
    * Removing samples.meta
    
    * returning back the skybox
    
    * fix: Added conditions that detect Safari on Mac OS
    
    added conditions that detect Safari on Mac OS to call `mediaDevices.getUserMedia`
    
    * doc: Added doc to integrate a turn server

---
+++ Packages/com.unity.renderstreaming/Samples~/HDRP/Editor/RenderStreamingHDRPUtility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.PackageManager;
+using System.Diagnostics; //StackTrace
+using System.IO; //Path
+
+public class RenderStreamingUtility
+{
+    // Add a menu item named "Do Something" to MyMenu in the menu bar.
+    [MenuItem("Assets/Render Streaming/Import Simple HDRP Sample")]
+    static void ImportHDRPSample()
+    {
+        UnityEditor.PackageManager.Client.Add("com.unity.render-pipelines.high-definition");
+
+        StackTrace st = new StackTrace(new StackFrame(true));
+        StackFrame sf = st.GetFrame(0);
+
+        string sampleDir = Path.GetDirectoryName(sf.GetFileName());
+        AssetDatabase.ImportPackage(sampleDir + "/RenderStreamingHDRPSimple.unitypackage",true);
+    }
+}
