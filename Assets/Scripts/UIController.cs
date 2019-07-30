bed28df8ab837e65f63caa4adb1378f28bf19dd5
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2019-07-30 18:10:24 +0900

    feat: Add UI to display keyboard typing (#86)
    * Add UI to display keyboard typing
    
    * Merged release branch
    
    * Removed the pragma warnings

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+namespace Unity.RenderStreaming
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI text = null;
+        [SerializeField] CanvasGroup canvasGroup = null;
+        [SerializeField] private AnimationCurve transitionCurve =
+            new AnimationCurve(
+                new Keyframe(0.75f, 1f, 0f, 0f),
+                new Keyframe(1f, 0f, 0f, 0f));
+
+        private float timeTransition;
+
+        void Start()
+        {
+            Keyboard.current.onTextInput += OnTextInput;
+            canvasGroup.alpha = 0;
+            text.text = string.Empty;
+        }
+
+        void FixedUpdate()
+        {
+            if (!Keyboard.current.anyKey.isPressed && !Mathf.Approximately(canvasGroup.alpha, 0f))
+            {
+                timeTransition += Time.deltaTime;
+                canvasGroup.alpha = transitionCurve.Evaluate(timeTransition);
+                if (Mathf.Approximately(canvasGroup.alpha, 0f))
+                {
+                    text.text = string.Empty;
+                }
+            }
+        }
+
+        void OnTextInput(char c)
+        {
+            canvasGroup.alpha = 1f;
+            text.text = c.ToString();
+            timeTransition = 0;
+        }
+    }
+}
