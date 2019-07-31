4842391e88ca0d10562072948991ff84b1a87b90
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-07-31 18:22:43 +0900

    Add ListRequestExtension to make it easier to perform operations on ListRequest.

---
+++ Packages/com.unity.renderstreaming/Editor/ListRequestExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor.PackageManager.Requests;  //ListRequest
+using UnityEditor.PackageManager;  
+using System.Collections.Generic;       //IEnumerable
+
+
+namespace Unity.RenderStreaming.Editor {
+    public static class ListRequestExtensions
+    {
+        public static PackageInfo FindPackage(this ListRequest listRequest, string packageName) {
+            IEnumerable<PackageInfo> packageInfoCollection = listRequest.Result as IEnumerable<PackageInfo>;
+            if (null == packageInfoCollection) {
+                return null;
+            }
+
+            var enumerator = packageInfoCollection.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                PackageInfo curInfo = enumerator.Current;
+                if (curInfo.name == packageName) {
+                    return curInfo;
+                }
+            }
+
+            return null;
+        }
+    }   
+}
