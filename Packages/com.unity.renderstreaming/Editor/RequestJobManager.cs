8e1f27ae294656b346d8c9127fa47376753c7fc1
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-01 15:09:08 +0900

    Simplify RequestJobManager by not using a static instance.

--- Packages/com.unity.renderstreaming/Editor/RequestJobManager.cs
+++ Packages/com.unity.renderstreaming/Editor/RequestJobManager.cs
@@ -11,7 +11,6 @@ public class RequestJobManager : ScriptableObject
 {
     [UnityEditor.InitializeOnLoadMethod]
     static void OnLoad() {
-        RequestJobManager jobManager = GetOrCreateInstance();
         UnityEditor.EditorApplication.update+=UpdateRequestJobs;        
     }
 
@@ -27,9 +26,8 @@ public class RequestJobManager : ScriptableObject
     public static void CreateListRequest(bool offlineMode, bool includeIndirectIndependencies,
        Action<Request<PackageCollection>> onSuccess, Action<Request<PackageCollection>> onFail)
     {
-        RequestJobManager jobManager = GetOrCreateInstance();
         ListRequest listReq = UnityEditor.PackageManager.Client.List(offlineMode, includeIndirectIndependencies);
-        jobManager.m_requestJobs.Add(new RequestJob<PackageCollection>(listReq,onSuccess,onFail));
+        m_requestJobs.Add(new RequestJob<PackageCollection>(listReq,onSuccess,onFail));
     }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -37,58 +35,38 @@ public class RequestJobManager : ScriptableObject
     public static void CreateAddRequest(string packageName,
         Action<Request<PackageInfo>> onSuccess, Action<Request<PackageInfo>> onFail)
     {
-        RequestJobManager jobManager = GetOrCreateInstance();
         AddRequest addReq = UnityEditor.PackageManager.Client.Add(packageName);
-        jobManager.m_requestJobs.Add(new RequestJob<PackageInfo>(addReq,onSuccess,onFail));
+        m_requestJobs.Add(new RequestJob<PackageInfo>(addReq,onSuccess,onFail));
     }
     
 //---------------------------------------------------------------------------------------------------------------------   
 
     static void UpdateRequestJobs()
     {
-        RequestJobManager jobManager = GetOrCreateInstance();
-
         {   //Update and register completed jobs
-            var enumerator = jobManager.m_requestJobs.GetEnumerator();
+            var enumerator = m_requestJobs.GetEnumerator();
             while (enumerator.MoveNext()) {
                 StatusCode code = enumerator.Current.Update();
                 if (StatusCode.Failure == code || StatusCode.Success == code) {
-                    jobManager.m_jobsToDelete.Add(enumerator.Current);
+                    m_jobsToDelete.Add(enumerator.Current);
                 }
             }
         }
         
         { //delete completed jobs
-            var enumerator = jobManager.m_jobsToDelete.GetEnumerator();
+            var enumerator = m_jobsToDelete.GetEnumerator();
             while (enumerator.MoveNext()) {
-                jobManager.m_requestJobs.Remove(enumerator.Current);
+                m_requestJobs.Remove(enumerator.Current);
             }
-            jobManager.m_jobsToDelete.Clear();
+            m_jobsToDelete.Clear();
         }
 
-    }
-
-//---------------------------------------------------------------------------------------------------------------------   
-
-    static RequestJobManager GetOrCreateInstance() {
-
-        if (null == m_instance) {
-            m_instance = UnityEngine.Object.FindObjectOfType<RequestJobManager>();
-
-            if (null == m_instance) {
-                m_instance = CreateInstance<RequestJobManager>();
-                return m_instance;
-            }
-        }
-        return m_instance;
     }
 
 //---------------------------------------------------------------------------------------------------------------------
 
-    System.Collections.Generic.HashSet<IRequestJob> m_requestJobs;
-    System.Collections.Generic.List<IRequestJob> m_jobsToDelete;
-
-    static RequestJobManager m_instance = null;
+    static System.Collections.Generic.HashSet<IRequestJob> m_requestJobs = new HashSet<IRequestJob>();
+    static System.Collections.Generic.List<IRequestJob> m_jobsToDelete = new List<IRequestJob>();
 
 }
 
