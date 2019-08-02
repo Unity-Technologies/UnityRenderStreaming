5d22304930ce41616acebcaa4f431f3537ddb309
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-02 14:43:34 +0900

    Add a unit test to test CreateListRequest

---
+++ Assets/Tests/Editor/RequestJobTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;                   //IEnumerator
+using UnityEngine;                          //Debug
+using UnityEngine.TestTools;                //UnityTest
+using Unity.RenderStreaming.Editor;         //RequestJobManager
+using UnityEditor.PackageManager.Requests;  //ListRequest, AddRequest, etc
+using UnityEditor.PackageManager;           //PackageCollection
+
+namespace Unity.RenderStreaming
+{
+public class RequestJobTest
+{
+    [UnityTest]
+    public IEnumerator VerifyRenderStreamingPackage() {
+
+        RequestJobManager.CreateListRequest(false, false, OnListRequestSucceeded, OnListRequestFailed);
+        while (!m_listRequestCompleted)
+            yield return null;
+
+        Debug.Assert(m_packageFound);
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+    void OnListRequestSucceeded(Request<PackageCollection> packageCollection) {
+        m_listRequestCompleted = true;
+        m_packageFound = null!=packageCollection.FindPackage("com.unity.renderstreaming");
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+    void OnListRequestFailed(Request<PackageCollection> packageCollection) {
+        m_listRequestCompleted = true;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    bool m_listRequestCompleted = false;
+    bool m_packageFound = false;
+
+}
+
+} //namespace Unity.RenderStreaming
