450cc6b556e216477707d6e9d583b3963014adc8
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-08-05 12:24:04 +0900

    feat: eliminates the necessity of user interaction when installing an HDRP sample (#97)
    * Import the HDRP Sample directly when RenderStreamingHDRPUtility is loaded
    
    * Add ListRequestExtension to make it easier to perform operations on ListRequest.
    
    * Renaming ListRequestExtensions to RequestExtensions
    
    And make the functions more generic
    
    * Add RequestJob and RequestJobManager to manage calls to PackageManager.Client
    
    * Using RequestJobManager in RenderStreamingHDRPUtility to simplify code
    
    * Simplify RequestJobManager by not using a static instance.
    
    * Add assembly def of RenderStreaming.Editor
    
    * Change RenderStreamingHDRPUtility into RenderStreamingHDRPAutomator that will "hack" Unity to execute code to install UnityPackage, if "Import In Project" is pressed
    
    * Showing ProgressBar a bit
    
    * Adding the metafile of the unitypackage sample so that the link in RenderStreamingHDRPAutomator is not broken
    
    * Fix the code flow so that we really execute the code when the program finished compiling, not when the ScriptableObject is enabled
    
    * Eliminate whitespace
    
    * Added API documentation
    
    * set the platform of the assembly of Unity.RenderStreaming.Editor

---
+++ Packages/com.unity.renderstreaming/Editor/RequestJobManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;           //HashSet
+using UnityEditor.PackageManager.Requests;  //ListRequest, AddRequest, etc
+using UnityEditor.PackageManager;           //PackageCollection
+using System;                               //Action
+
+
+namespace Unity.RenderStreaming.Editor {
+
+
+/// <summary>
+/// An editor class to manage requests to UnityEditor.PackageManager.Client
+/// This class will perform its operations in background while Unity is running.
+/// </summary>
+public class RequestJobManager 
+{
+    [UnityEditor.InitializeOnLoadMethod]
+    static void OnLoad() {    
+        UnityEditor.EditorApplication.update+=UpdateRequestJobs;        
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Queue a job to create a ListRequest instance
+    /// </summary>
+    /// <param name="offlineMode">Specifies whether or not the Package Manager requests the latest information about
+    ///     the project's packages from the remote Unity package registry. When offlineMode is true,
+    ///     the PackageInfo objects in the PackageCollection returned by the Package Manager contain information
+    ///     obtained from the local package cache, which could be out of date.</param>
+    /// <param name="includeIndirectIndependencies">Set to true to include indirect dependencies in the
+    ///     PackageCollection returned by the Package Manager. Indirect dependencies include packages referenced
+    ///     in the manifests of project packages or in the manifests of other indirect dependencies. Set to false
+    ///     to include only the packages listed directly in the project manifest.</param>
+    /// <param name="onSuccess">Action which is executed if the request succeeded</param>
+    /// <param name="onFail">Action which is executed if the request failed </param>
+    /// 
+    public static void CreateListRequest(bool offlineMode, bool includeIndirectIndependencies,
+       Action<Request<PackageCollection>> onSuccess, Action<Request<PackageCollection>> onFail)
+    {
+        m_pendingListRequests.Enqueue(new ListRequestInfo(offlineMode, includeIndirectIndependencies, onSuccess, onFail));
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Queue a job to create an AddRequest instance
+    /// </summary>
+    /// <param name="packageName">The name or ID of the package to add. If only the name is specified,
+    ///     the latest version of the package is installed.</param>
+    /// <param name="onSuccess">Action which is executed if the request succeeded</param>
+    /// <param name="onFail">Action which is executed if the request failed </param>
+    /// 
+    public static void CreateAddRequest(string packageName,
+        Action<Request<PackageInfo>> onSuccess, Action<Request<PackageInfo>> onFail)
+    {
+        m_pendingAddRequests.Enqueue(new AddRequestInfo(packageName, onSuccess, onFail));
+    }
+    
+//---------------------------------------------------------------------------------------------------------------------   
+
+    static void UpdateRequestJobs()
+    {
+        {   //Process pending list requests
+            var enumerator = m_pendingListRequests.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                ListRequestInfo info = enumerator.Current;
+                ListRequest listReq = Client.List(info.OfflineMode, info.IncludeIndirectIndependencies);
+                m_requestJobs.Add(new RequestJob<PackageCollection>(listReq,info.OnSuccessAction,info.OnFailAction));               
+            }
+            m_pendingListRequests.Clear();
+        }
+
+        {   //Process pending addrequests
+            var enumerator = m_pendingAddRequests.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                AddRequestInfo info = enumerator.Current;
+                AddRequest addReq = Client.Add(info.PackageName);
+                m_requestJobs.Add(new RequestJob<PackageInfo>(addReq,info.OnSuccessAction,info.OnFailAction));
+            }
+            m_pendingAddRequests.Clear();
+        }
+
+        {   //Update and register completed jobs
+            var enumerator = m_requestJobs.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                StatusCode code = enumerator.Current.Update();
+                if (StatusCode.Failure == code || StatusCode.Success == code) {
+                    m_jobsToDelete.Add(enumerator.Current);
+                }
+            }
+        }
+        
+        { //delete completed jobs
+            var enumerator = m_jobsToDelete.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                m_requestJobs.Remove(enumerator.Current);
+            }
+            m_jobsToDelete.Clear();
+        }
+
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static Queue<ListRequestInfo> m_pendingListRequests = new Queue<ListRequestInfo>();
+    static Queue<AddRequestInfo>  m_pendingAddRequests = new Queue<AddRequestInfo>();
+
+    static System.Collections.Generic.HashSet<IRequestJob> m_requestJobs = new HashSet<IRequestJob>();
+    static System.Collections.Generic.List<IRequestJob> m_jobsToDelete = new List<IRequestJob>();
+
+}
+
+} //namespace Unity.RenderStreaming.Editor 
---
+++ Packages/com.unity.renderstreaming/Samples~/HDRP/Editor/RenderStreamingHDRPAutomator.cs
@@ -0,0 +1,105 @@
+﻿using UnityEditor.PackageManager.Requests;  //Request
+using UnityEditor.PackageManager;           //PackageCollection
+using Unity.RenderStreaming.Editor;         //RequestJobManager
+using System.IO;                            //Path
+using UnityEngine;                          //ScriptableObject
+using System.Diagnostics;                   //StackTrace
+
+public class RenderStreamingHDRPAutomator : ScriptableObject 
+{
+    [UnityEditor.InitializeOnLoadMethod]
+    static public void OnLoad() {
+        //Some steps are necessary to "hack" so that Unity will execute this file everytime we click "Import in project"
+        //In the package manager UI.
+        //1. Import the ScriptableObject to make sure that we are dealing with the new copied asset, and not the asset
+        //   modified later in this module
+        //2. Change the C# code (this file) a bit to trigger C# compilation, even though the file content is the same
+        //3. One additional requirement that the asset must be in the same path as this C# file
+
+        Init();
+        UnityEditor.AssetDatabase.ImportAsset(m_automatorAssetPath);
+
+        if (!string.IsNullOrEmpty(m_automatorAsset.m_version)) {
+            return;
+        }
+        
+        RequestJobManager.CreateListRequest(false, true, OnPackageListRequestSuccess, null);
+        UnityEditor.EditorUtility.DisplayProgressBar(PROGRESS_BAR_TITLE, PROGRESS_BAR_INFO, 0.1f );
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static void OnPackageListRequestSuccess(Request<PackageCollection> req) {
+
+        //Check if HDRP hasn't been installed and add it if that's the case
+        const string HDRP_PACKAGE_NAME = "com.unity.render-pipelines.high-definition";
+        PackageInfo packageInfo = req.FindPackage(HDRP_PACKAGE_NAME);
+        if (null == packageInfo) {
+            RequestJobManager.CreateAddRequest(HDRP_PACKAGE_NAME, OnHDRPPackageAdded, null);
+        } else {
+            ImportHDRPSample();
+        }
+
+        //update ScriptableObject asset
+        PackageInfo renderStreamingPackageInfo = req.FindPackage("com.unity.renderstreaming");
+        if (null != renderStreamingPackageInfo) {
+            m_automatorAsset.m_version = renderStreamingPackageInfo.version;
+        }
+
+        //Force saving the ScriptableObject
+        string[] assetPaths = { m_automatorAssetPath };
+        UnityEditor.AssetDatabase.ForceReserializeAssets(assetPaths, UnityEditor.ForceReserializeAssetsOptions.ReserializeAssets);
+
+        //Change the C# file to trigger recompilation next time "Import in project" is pushed again
+        string cScript = Path.ChangeExtension(m_automatorAssetPath, ".cs");
+        File.AppendAllText(cScript, System.Environment.NewLine + "//Automatically Modified to trigger recompilation");
+        UnityEditor.AssetDatabase.ImportAsset(cScript);
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static void OnHDRPPackageAdded(Request<PackageInfo> req) {
+        ImportHDRPSample();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static void ImportHDRPSample() {
+        UnityEditor.EditorUtility.DisplayProgressBar(PROGRESS_BAR_TITLE, PROGRESS_BAR_INFO, 0.8f );
+
+        string path = UnityEditor.AssetDatabase.GetAssetPath(m_automatorAsset.m_samplePackage);
+        UnityEditor.AssetDatabase.ImportPackage(path, true);
+
+        UnityEditor.EditorUtility.ClearProgressBar();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    static void Init() {
+
+        StackTrace st = new StackTrace(new StackFrame(true));
+        StackFrame sf = st.GetFrame(0);
+
+        string path = Path.ChangeExtension(sf.GetFileName(),".asset").Replace(@"\", "/" );
+
+        //Change absolute path to relative path
+        if (path.StartsWith(Application.dataPath)) {
+            path=  "Assets" + path.Substring(Application.dataPath.Length);
+        }
+        m_automatorAssetPath = path;
+        m_automatorAsset = UnityEditor.AssetDatabase.LoadAssetAtPath<RenderStreamingHDRPAutomator>(m_automatorAssetPath);
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private string m_version = null;
+    [SerializeField] Object m_samplePackage = null;
+
+    static string m_automatorAssetPath;
+    static RenderStreamingHDRPAutomator m_automatorAsset;
+
+    readonly static string PROGRESS_BAR_TITLE = "RenderStreaming";
+    readonly static string PROGRESS_BAR_INFO  = "Installing HDRP Sample";
+
+}
+
