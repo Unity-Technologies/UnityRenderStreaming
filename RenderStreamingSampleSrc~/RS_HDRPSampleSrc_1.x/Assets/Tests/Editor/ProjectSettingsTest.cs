1e9e095528de56051b39ee6bb5e116ba9856b46e
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-08 14:53:37 +0900

    Add tests to verify project settings

---
+++ RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/Tests/Editor/ProjectSettingsTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UnityEngine;                  //ColorSpace
+using UnityEngine.Rendering;        //GraphicsSettings
+using UnityEditor;                  //PlayerSettings
+
+namespace Tests.Editor
+{
+
+public class ProjectSettingsTest {
+    [Test]
+    public void VerifyGraphicsSettings()  {
+        Assert.True(null != GraphicsSettings.renderPipelineAsset);
+        Assert.True(ColorSpace.Linear == PlayerSettings.colorSpace );
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    [Test]
+    public void VerifyPlayerSettings()  {
+        Assert.True(ColorSpace.Linear == PlayerSettings.colorSpace );
+    }
+}
+
+} //namespace Tests.Editor
