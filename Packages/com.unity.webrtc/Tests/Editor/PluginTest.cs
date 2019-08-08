4a2b2566949b81872b45ae81216102156540ac89
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2019-08-08 18:47:32 +0900

    test: make sure that webrtc.dll is successfully loaded (#104)
    * Exclude imported samples from git
    
    * Removing empty classes
    
    * Add PluginTest to ensure that the DLL is successfully loaded

---
+++ Packages/com.unity.webrtc/Tests/Editor/PluginTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;   //Process
+using NUnit.Framework;      //Assert
+
+namespace Unity.WebRTC.Editor {
+
+public class PluginTest {
+    [Test]
+    public static void IsPluginLoaded() {
+        // Get the current process.
+        Process currentProcess = Process.GetCurrentProcess();
+        bool found = false;
+        var enumerator = currentProcess.Modules.GetEnumerator();
+        while (enumerator.MoveNext()) {
+            ProcessModule module = enumerator.Current as ProcessModule;
+            if (null != module && module.ModuleName=="webrtc.dll")
+                found = true;
+
+        }
+        Assert.True(found);
+    }
+}
+
+} //namespace Unity.WebRTC.Editor
