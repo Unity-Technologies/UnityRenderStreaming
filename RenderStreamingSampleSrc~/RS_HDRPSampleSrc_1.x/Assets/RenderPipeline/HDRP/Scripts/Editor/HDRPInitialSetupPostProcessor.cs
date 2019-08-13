0e6ab59e0e82a3191c123a10df6e07b96510e31e
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-13 21:20:33 +0900

    Debugging ! And updating unitypackage
    (cherry picked from commit 21f597ebf12775f1a25e725a8c06bb683496b394)

--- RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/RenderPipeline/HDRP/Scripts/Editor/HDRPInitialSetupPostProcessor.cs
+++ RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/RenderPipeline/HDRP/Scripts/Editor/HDRPInitialSetupPostProcessor.cs
@@ -8,6 +8,7 @@ public class HDRPPostProcessor : AssetPostprocessor
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
         string[] movedAssets, string[] movedFromAssetPaths)
     {
+        Debug.Log("HDRPPostProcessor::OnPostprocessAllAssets");
         if (null != GraphicsSettings.renderPipelineAsset) {
             return;
         }
@@ -16,13 +17,18 @@ public class HDRPPostProcessor : AssetPostprocessor
         for (int i=0;i< numImportedAssets; ++i) {
             string curAssetPath = importedAssets[i];
 
+            Debug.Log("     Running---- " + curAssetPath);
             if (curAssetPath == "Assets/RenderPipeline/HDRP/HDRenderPipelineAsset.asset") {
                 HDRenderPipelineAsset pipelineAsset = AssetDatabase.LoadAssetAtPath<HDRenderPipelineAsset>(curAssetPath);
                 GraphicsSettings.renderPipelineAsset = pipelineAsset;
 
                 PlayerSettings.colorSpace = ColorSpace.Linear;
-                AssetDatabase.SaveAssets();
+                Debug.Log("     ColorSpace: " + PlayerSettings.colorSpace.ToString());
+
             }
         }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
     }
 }
