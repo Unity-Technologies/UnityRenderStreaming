6754ca6cdc99b3659113ab8068e3d97900424749
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-13 16:51:05 +0900

    Eliminate TestResults parameter from exec_unity_method.
    It's not a unit test, and therefore there won't be any test results.

--- exec_unity_method.cmd
+++ exec_unity_method.cmd
@@ -4,8 +4,7 @@ set unityPath=%1
 set projectPath=%2
 set methodName=%3
 set editorLogPath=%4
-set testResultPath=%5
 
 echo Calling Unity Method %methodName%
-%unityPath%  -projectPath %projectPath% -executeMethod %methodName% -logFile %editorLogPath% -editorTestsResultFile %testResultPath% -quit -batchmode 
+%unityPath%  -projectPath %projectPath% -executeMethod %methodName% -logFile %editorLogPath% -quit -batchmode 
 
