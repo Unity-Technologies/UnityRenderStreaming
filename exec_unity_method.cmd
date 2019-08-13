b36d234c3a6d66ea94771818650506c1fa0e8e61
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-13 14:40:12 +0900

    Execute an additional method after the dummy test step to import the HDRP sample explicity

---
+++ exec_unity_method.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+set unityPath=%1
+set projectPath=%2
+set methodName=%3
+set editorLogPath=%4
+set testResultPath=%5
+
+echo Calling Unity Method %methodName%
+%unityPath%  -projectPath %projectPath% -executeMethod %methodName% -logFile %editorLogPath% -editorTestsResultFile %testResultPath%
+
