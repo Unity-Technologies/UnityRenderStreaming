24848da6176cb73a7087e4d37bc44edc410f63f7
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-08-14 14:29:46 +0900

    Remove debug code

--- RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/RenderPipeline/HDRP/Scripts/Editor/HDRPInitialSetupPostProcessor.cs
+++ RenderStreamingSampleSrc~/RS_HDRPSampleSrc_1.x/Assets/RenderPipeline/HDRP/Scripts/Editor/HDRPInitialSetupPostProcessor.cs
@@ -8,7 +8,6 @@ public class HDRPPostProcessor : AssetPostprocessor
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
         string[] movedAssets, string[] movedFromAssetPaths)
     {
-        Debug.Log("HDRPPostProcessor::OnPostprocessAllAssets");
         if (null != GraphicsSettings.renderPipelineAsset) {
             return;
         }
@@ -17,18 +16,12 @@ public class HDRPPostProcessor : AssetPostprocessor
         for (int i=0;i< numImportedAssets; ++i) {
             string curAssetPath = importedAssets[i];
 
-            Debug.Log("     Running---- " + curAssetPath);
             if (curAssetPath == "Assets/RenderPipeline/HDRP/HDRenderPipelineAsset.asset") {
                 HDRenderPipelineAsset pipelineAsset = AssetDatabase.LoadAssetAtPath<HDRenderPipelineAsset>(curAssetPath);
                 GraphicsSettings.renderPipelineAsset = pipelineAsset;
 
                 PlayerSettings.colorSpace = ColorSpace.Linear;
-                Debug.Log("     ColorSpace: " + PlayerSettings.colorSpace.ToString());
-
             }
         }
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-
     }
 }
