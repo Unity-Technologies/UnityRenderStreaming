513575b6958d405dff954929176afd8383ffcf86
kazuki matsumoto <karasusan@gmail.com>
2019-08-18 16:35:07 -0700

    Added menu class

---
+++ Assets/Scripts/Editor/RenderStreamingMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Unity.RenderStreaming.Editor
+{
+    public static class RenderStreamingMenu
+    {
+        const string URLWebApp = "https://github.com/Unity-Technologies/UnityRenderStreaming/releases/download/{0}/webserver";
+
+        [MenuItem("Tools/Render Streaming/Download web app")]
+        static void DownloadWepApp()
+        {
+            //  string.Format(URLWebApp, packageVersion);
+        }
+    }
+}
+
