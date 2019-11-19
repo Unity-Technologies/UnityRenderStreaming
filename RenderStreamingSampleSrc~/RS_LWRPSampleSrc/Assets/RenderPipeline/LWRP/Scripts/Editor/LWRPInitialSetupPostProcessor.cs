98adca1f60533b32b847aab7645565a0d744f4d3
Sindharta Tanuwijaya <sindharta@gmail.com>
2019-11-19 15:17:18 +0900

    Setting up LWRP render pipeline

---
+++ RenderStreamingSampleSrc~/RS_LWRPSampleSrc/Assets/RenderPipeline/LWRP/Scripts/Editor/LWRPInitialSetupPostProcessor.cs
@@ -0,0 +1,27 @@
+﻿﻿using UnityEngine;                                      //Debug
+using UnityEditor;                                      //AssetPostProcessor
+using UnityEngine.Rendering;                            //GraphicsSettings
+using UnityEngine.Rendering.LWRP;
+
+public class HDRPInitialSetupPostProcessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
+        string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        if (null != GraphicsSettings.renderPipelineAsset) {
+            return;
+        }
+        
+        int numImportedAssets = importedAssets.Length;
+        for (int i=0;i< numImportedAssets; ++i) {
+            string curAssetPath = importedAssets[i];
+
+            if (curAssetPath == "Assets/RenderPipeline/LWRP/LightweightRenderPipelineAsset.asset") {
+                LightweightRenderPipelineAsset pipelineAsset =
+                    AssetDatabase.LoadAssetAtPath<LightweightRenderPipelineAsset>(curAssetPath);
+                GraphicsSettings.renderPipelineAsset = pipelineAsset;
+                PlayerSettings.colorSpace = ColorSpace.Linear;
+            }
+        }
+    }
+}
