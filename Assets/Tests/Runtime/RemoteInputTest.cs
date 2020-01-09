3c62b3d5b1d07de532b5abde7433502d20a2e9cf
kazuki matsumoto <karasusan@gmail.com>
2020-01-09 16:49:49 +0900

    refactor

--- Assets/Tests/Runtime/RemoteInputTest.cs
+++ Assets/Tests/Runtime/RemoteInputTest.cs
@@ -1,27 +1,10 @@
-﻿using System.Collections;
-using NUnit.Framework;
-using UnityEngine.TestTools;
-using UnityEngine;
+﻿using NUnit.Framework;
 
 namespace Unity.RenderStreaming
 {
 
     public class RemoteInputTest
     {
-        GameObject CreateMainCamera()
-        {
-            var obj = new GameObject("Camera");
-            var camera = obj.AddComponent<Camera>();
-            camera.tag = "MainCamera";
-            return obj;
-        }
-
-        [SetUp]
-        public void SetUp()
-        {
-            CreateMainCamera();
-        }
-
         [Test]
         public void InitializeAndDestroy()
         {
