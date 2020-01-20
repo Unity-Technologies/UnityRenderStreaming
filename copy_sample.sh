d9fc2c2241f3b7d3f060ab1d411fc75031dd8a58
kazuki matsumoto <karasusan@gmail.com>
2020-01-20 12:26:49 +0900

    fixed build error

--- copy_sample.sh
+++ copy_sample.sh
@@ -2,11 +2,10 @@ packageName=$1
 sampleName=$2
 
 echo Copying Packages
-mkdir -p EmptyProjectForTest~/Packages/com.unity.renderstreaming
-cp -r Packages/com.unity.renderstreaming EmptyProjectForTest~/Packages/com.unity.renderstreaming
+cp -r Packages/com.unity.renderstreaming EmptyProjectForTest~/Packages
 
 echo Copying ${sampleName} Sample from ${packageName}
-mkdir -p EmptyProjectForTest~/Assets/Samples/${sampleName}
-cp -r Packages/${packageName}/Samples~/${sampleName} EmptyProjectForTest~/Assets/Samples/${sampleName}
+mkdir -p EmptyProjectForTest~/Assets/Samples
+cp -r Packages/${packageName}/Samples~/${sampleName} EmptyProjectForTest~/Assets/Samples
 
-echo Copying %sampleName% Finished
+echo Copying ${sampleName} Finished
