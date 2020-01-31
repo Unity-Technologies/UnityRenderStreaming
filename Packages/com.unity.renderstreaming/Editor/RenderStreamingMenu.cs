428044ceb0a4c330eb38c4738fb3a052f959573d
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2020-01-31 14:26:48 +0900

    refactor: Prepared template testing (#218)
    * add patch file to replace "manifest.json" for testing template
    
    * add template unittest
    
    * exclude RenderStreaming test on MacOS
    
    * move the place where web application executable file
    
    * cp WebApp folder to resolve the path searching of the webserver
    
    * added method "ChangeGraphicsApi"
    
    * fixed build script to copy webserver
    
    * fixed build error
    
    * exclude macos platform from template test
    
    * test
    
    * test
    
    * add meta
    
    * test
    
    * test
    
    * remove trigger to stop template test
    
    * fixed warning
    
    * upgrade input system 1.0.0-preview.4
    
    * fixed comment

--- Packages/com.unity.renderstreaming/Editor/RenderStreamingMenu.cs
+++ Packages/com.unity.renderstreaming/Editor/RenderStreamingMenu.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
@@ -8,115 +8,28 @@ namespace Unity.RenderStreaming.Editor
 {
     public static class RenderStreamingMenu
     {
-        const string URLRoot = "https://github.com/Unity-Technologies/UnityRenderStreaming";
-        const string LatestKnownVersion = "1.1.1-preview";
-
-        // TODO::fix release process of webserver runtime.
-        const string PathWebAppForMac = "releases/download/{0}/webserver";
-        const string PathWebAppForLinux = "releases/download/{0}/webserver";
-        const string PathWebAppForWin = "releases/download/{0}/webserver.exe";
-        //
-
-        const string PathWebAppSourceCode = "tree/release/{0}/WebApp";
-        const string PathWebAppDocumentation = "blob/release/{0}/Packages/com.unity.template.renderstreaming/Documentation~/en/webserver.md";
-
         [MenuItem("Edit/Render Streaming/Download web app")]
-        static void DownloadWebApp() {
-            RequestPackageVersion("com.unity.renderstreaming", (version) => { DownloadWebApp(version); });
-        }
-
-        static void DownloadWebApp(string version)
-
-        {
-#if UNITY_EDITOR_WIN
-            var url = System.IO.Path.Combine(URLRoot, string.Format(PathWebAppForWin, version));
-#elif UNITY_EDITOR_OSX
-            var url = System.IO.Path.Combine(URLRoot, string.Format(PathWebAppForMac, version));
-#elif UNITY_EDITOR_LINUX
-            var url = System.IO.Path.Combine(URLRoot, string.Format(PathWebAppForLinux, version));
-#endif
-            var client = new WebClient();
-            var filename = System.IO.Path.GetFileName(url);
-            var tmpFilePath = System.IO.Path.Combine(Application.temporaryCachePath, filename);
-
-            client.DownloadFileCompleted += (object sender, System.ComponentModel.AsyncCompletedEventArgs e) =>
-            {
-                EditorUtility.ClearProgressBar();
-                if (e.Error != null) {
-                    //Try downloading using the latest known version to work.
-                    if (version != LatestKnownVersion) {
-                        DownloadWebApp(LatestKnownVersion);
-                    } else {
-                        Debug.LogError("Failed downloading webserver from: " + url + " . Error: " + e.Error.ToString());
-                    }
-                    return;
-                }
-
-                if (!System.IO.File.Exists(tmpFilePath))
-                {
-                    Debug.LogErrorFormat("Download failed. url:{0}", url);
-                    return;
-                }
-
+        static void DownloadWebAppFromMenu() {
+            WebAppDownloader.GetPackageVersion("com.unity.renderstreaming", (version) => {
                 var dstPath = EditorUtility.OpenFolderPanel("Select download folder", "", "");
-                if (string.IsNullOrEmpty(dstPath))
-                {
-                    return;
-                }
-                dstPath = System.IO.Path.Combine(dstPath, filename);
-                if (System.IO.File.Exists(dstPath))
-                {
-                    System.IO.File.Delete(dstPath);
-                }
-                System.IO.File.Move(tmpFilePath, dstPath);
-                EditorUtility.RevealInFinder(dstPath);
-            };
-            client.DownloadProgressChanged += (object sender, DownloadProgressChangedEventArgs e) =>
-            {
-                var progress = e.ProgressPercentage / 100f;
-                if(EditorUtility.DisplayCancelableProgressBar("Downloading", url, progress))
-                {
-                    client.CancelAsync();
-                }
-            };
-            client.DownloadFileAsync(new System.Uri(url), tmpFilePath);
-            
-        }
-
-        static void RequestPackageVersion(string packageName, System.Action<string> callback)
-        {
-            // request package list to get package version
-            RequestJobManager.CreateListRequest(false, true, (req) =>
-            {
-                var packageInfo = req.FindPackage(packageName);
-                if (null == packageInfo)
-                {
-                    Debug.LogErrorFormat("Not found package \"{0}\"", packageName);
-                    return;
-                }
-                callback(packageInfo.version);
-            }, null);
+                WebAppDownloader.DownloadWebApp(version, dstPath);
+            });
         }
 
         [MenuItem("Edit/Render Streaming/Show web app documentation")]
         static void ShowWepAppDocumentation()
         {
-            RequestPackageVersion("com.unity.renderstreaming", (version) =>
-            {
-                var pattern = @"\d+.\d+.\d+";
-                var result = System.Text.RegularExpressions.Regex.Match(version, pattern);
-                var url = System.IO.Path.Combine(URLRoot, string.Format(PathWebAppDocumentation, result.Value));
+            WebAppDownloader.GetPackageVersion("com.unity.renderstreaming", (version) => {
+                var url = WebAppDownloader.GetURLDocumentation(version);
                 Application.OpenURL(url);
             });
         }
+
         [MenuItem("Edit/Render Streaming/Show web app source code")]
         static void ShowWepAppSourceCode()
         {
-            RequestPackageVersion("com.unity.renderstreaming", (version) =>
-            {
-                var pattern = @"\d+.\d+.\d+";
-                var result = System.Text.RegularExpressions.Regex.Match(version, pattern);
-                var url = System.IO.Path.Combine(URLRoot, string.Format(PathWebAppSourceCode, result.Value));
+            WebAppDownloader.GetPackageVersion("com.unity.renderstreaming", (version) => {
+                var url = WebAppDownloader.GetURLSourceCode(version);
                 Application.OpenURL(url);
             });
         }
