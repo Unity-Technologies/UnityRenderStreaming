b28b081ae234e54dd46c092a0b06c89f8828246c
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2020-02-05 17:26:48 +0900

    feat: added AudioStreamer (#222)
    * Added AudioStreamer
    
    * move files
    
    * upgrade webrtc package version
    
    * Fixed sample scene

---
+++ Assets/Scripts/AudioStreamer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.RenderStreaming
+{
+    [RequireComponent(typeof(AudioListener))]
+    public class AudioStreamer : MonoBehaviour
+    {
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            WebRTC.Audio.Update(data, channels);
+        }
+    }
+
+}
