029b087f6aa92785ead6a00a6fc289196f78d0c4
TakashiKannan <wingrakhi@live.jp>
2020-03-16 09:54:19 +0900

    update shell cmd path

--- copy_sample.sh
+++ copy_sample.sh
@@ -2,10 +2,10 @@ packageName=$1
 sampleName=$2
 
 echo Copying Packages
-cp -r Packages/com.unity.renderstreaming EmptyProjectForTest~/Packages
+cp -r com.unity.renderstreaming EmptyProjectForTest~/Packages
 
 echo Copying ${sampleName} Sample from ${packageName}
 mkdir -p EmptyProjectForTest~/Assets/Samples
-cp -r Packages/${packageName}/Samples~/${sampleName} EmptyProjectForTest~/Assets/Samples
+cp -r ${packageName}/Samples~/${sampleName} EmptyProjectForTest~/Assets/Samples
 
 echo Copying ${sampleName} Finished
