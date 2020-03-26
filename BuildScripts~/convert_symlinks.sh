f3a0bd70bc7ea52ca81894158a8fa929b1fd1eac
sindharta <sindharta@gmail.com>
2020-03-26 16:35:54 +0900

    escape path with space using {}

--- BuildScripts~/convert_symlinks.sh
+++ BuildScripts~/convert_symlinks.sh
@@ -1,9 +1,11 @@
-TEMP_SYMLINK="$1Temp" 
+
+ESCAPED_PATH=${1}
+TEMP_SYMLINK="${ESCAPED_PAT}Temp" 
 
 echo
 echo --------------------------------------------------------------------------
 echo Converting symlinks to copies: $1 
 echo --------------------------------------------------------------------------
-mv $1 $TEMP_SYMLINK
-rsync $TEMP_SYMLINK/ $1/ -a --copy-links -v
-rm $TEMP_SYMLINK
+mv "${ESCAPED_PATH}" "$TEMP_SYMLINK"
+rsync "${TEMP_SYMLINK}/" "${ESCAPED_PATH}/" -a --copy-links -v
+rm "${TEMP_SYMLINK}"
