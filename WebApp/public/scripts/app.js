d414690c06460abc5fe6df18b32f4afda4980429
Luc Vo Van <l.vovan@gmail.com>
2020-04-03 06:48:27 -0400

    Gamepad support (#197)
    * Gamepad support
    
    * fixing missing parenthesis
    
    * fixing index clash with different users, merging triggers with buttons
    
    * rename enums
    
    * change line ending
    
    Co-authored-by: gewentao <sailbear@foxmail.com>
    Co-authored-by: kazuki <karasusan@gmail.com>

--- WebApp/public/scripts/app.js
+++ WebApp/public/scripts/app.js
@@ -1,5 +1,6 @@
 import { VideoPlayer } from "./video-player.js";
-import { registerKeyboardEvents, registerMouseEvents, sendClickEvent } from "./register-events.js";
+import { registerGamepadEvents, registerKeyboardEvents, registerMouseEvents, sendClickEvent } from "./register-events.js";
+import { startGamepadDetection } from "./gamepadEvents.js";
 
 let playButton;
 let videoPlayer;
@@ -99,9 +100,11 @@ async function setupVideoPlayer(element, config) {
   await videoPlayer.setupConnection();
 
   videoPlayer.ondisconnect = onDisconnect;
+  registerGamepadEvents(videoPlayer);
   registerKeyboardEvents(videoPlayer);
   registerMouseEvents(videoPlayer, element);
-
+  
+  startGamepadDetection();
   return videoPlayer;
 }
 
