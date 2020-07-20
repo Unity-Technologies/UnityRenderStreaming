e8fac76d4549af592cffb37b1af56f8421642c54
Takashi KANNAN <takashi.kannan@unity3d.com>
2020-07-20 15:58:46 +0900

    fix shell

--- pack_webapp.sh
+++ pack_webapp.sh
@@ -2,7 +2,16 @@ cd WebApp
 npm install
 npm run build
 npm run pack
+
+app_name=webserver
+
+if [ "$(uname)" == 'Darwin' ]; then
+  app_name=webserver_mac
+fi
+
 chmod a+x webserver
+mv webserver $app_name
+
 cd ..
 mkdir WebApp/bin~
-mv WebApp/webserver WebApp/bin~
+mv WebApp/$app_name WebApp/bin~
--- pack_webapp_mac.sh
+++
@@ -1,9 +0,0 @@
-cd WebApp
-npm install
-npm run build
-npm run pack
-mv webserver webserver_mac
-chmod a+x webserver_mac
-cd ..
-mkdir WebApp/bin~
-mv WebApp/webserver_mac WebApp/bin~
