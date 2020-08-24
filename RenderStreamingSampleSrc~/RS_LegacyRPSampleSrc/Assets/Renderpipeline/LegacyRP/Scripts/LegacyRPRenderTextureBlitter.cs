7df30bfcb0ca0b733ee5d82a8311b305ef05374b
Takashi Kannan <26959415+kannan-xiao4@users.noreply.github.com>
2020-08-24 08:11:27 +0000

    fix: legacyrp blitter (#322)
    * fix legacyrp blitter
    
    * update sample unitypackage

---
+++ RenderStreamingSampleSrc~/RS_LegacyRPSampleSrc/Assets/Renderpipeline/LegacyRP/Scripts/LegacyRPRenderTextureBlitter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class LegacyRPRenderTextureBlitter : MonoBehaviour
+{
+    [SerializeField] Camera m_rtCamera = null;
+
+    Camera m_cam;
+
+    private void OnEnable()
+    {
+        m_cam = GetComponent<Camera>();
+
+        //Render nothing
+        m_cam.clearFlags = CameraClearFlags.Nothing;
+        m_cam.cullingMask = 0;
+
+        Camera.onPreRender += BlitTexture;
+    }
+
+    private void OnDisable()
+    {
+        Camera.onPreRender -= BlitTexture;
+    }
+
+    private void BlitTexture(Camera cam)
+    {
+        if (m_cam == cam && null != m_rtCamera.targetTexture)
+        {
+            Graphics.Blit(m_rtCamera.targetTexture, (RenderTexture)null);
+        }
+    }
+}
