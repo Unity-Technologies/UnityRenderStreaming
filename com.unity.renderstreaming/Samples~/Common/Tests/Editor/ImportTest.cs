59ad029cb780a565a0885bb0e4a2685d28d32792
takashikannan <takashi.kannan@unity3d.com>
2020-12-09 14:54:31 +0900

    add common unitypackage

---
+++ com.unity.renderstreaming/Samples~/Common/Tests/Editor/ImportTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;                   //IEnumerator
+using NUnit.Framework;                      //Timeout
+using UnityEngine.TestTools;                //UnityTest
+
+public class ImportTest
+{
+    [UnityTest]
+    [Timeout(3600000)] //1 hour
+    public IEnumerator ImportCommonSampleTest() {
+
+        RenderStreamingCommonAutomator.TryAddCommonPackageAndImportSample();
+        while (!RenderStreamingCommonAutomator.IsSampleImported()) {
+            yield return null;
+        }
+    }
+}
+
