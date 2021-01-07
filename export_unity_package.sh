62dc48d5f8a575e1976dba0ea147dfd2c660abc1
takashikannan <takashi.kannan@unity3d.com>
2021-01-07 15:12:03 +0900

    delete no need cmmand script and fix yaml

--- export_unity_package.sh
+++
@@ -1,7 +0,0 @@
-unityPath=$1
-projectPath=$2
-packagePath=$3
-editorLogPath=$4
-
-echo Exporting Package: ${packagePath}
-${unityPath} -batchmode -nographics -quit -projectPath ${projectPath} -exportPackage "Assets" ${packagePath} -logFile ${editorLogPath}
--- run_editor_tests.sh
+++
@@ -1,8 +0,0 @@
-unityPath=$1
-projectPath=$2
-editorLogPath=$3
-testResultPath=$4
-
-echo Running Editor Tests
-${unityPath}  -projectPath ${projectPath} -batchmode -runEditorTests -logFile ${editorLogPath} -editorTestsResultFile ${testResultPath}
-
