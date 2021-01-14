b4416dc8f147b42c22631542607fcb3cfa0ca0d7
Takashi Kannan <26959415+kannan-xiao4@users.noreply.github.com>
2021-01-14 10:22:11 +0900

    refactor: Move script to renderstreaming package (#373)
    * move renderstreaming editor script
    
    * move renderstreaming runtime script
    
    * move renderstreaming test script
    
    * fix assembly definition
    
    * update manifest and delete symbolic link
    
    * delete old sample folder
    
    * move sample asset to renderstreaming package sample folder
    
    * delete old sample source
    
    * move renderpipeline script to package
    
    * fix assembly definition about renderpipeline
    
    * fix postprocess rederpipeline setting
    
    * fix yamate yml
    
    * delete no need cmmand script and fix yaml
    
    * add dummy test
    
    * fix initialize import renderpipeline script
    
    * fix package test ci
    
    * use metal device on macos
    
    * change signaling server port number on test
    
    * use packageci image on mac
    
    * skip signaling test on linux
    
    * skip rtx template test on not windows
    
    * skip signaling test on mac

--- export_unity_package.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set unityPath=%1
-set projectPath=%2
-set packagePath=%3
-set editorLogPath=%4
-
-echo Exporting Package: %packagePath%
-%unityPath% -batchmode -nographics -quit -projectPath %projectPath% -exportPackage "Assets" %packagePath% -logFile %editorLogPath%
--- import_unity_package.sh
+++
@@ -1,7 +0,0 @@
-unityPath=$1
-projectPath=$2
-packagePath=$3
-editorLogPath=$4
-
-echo Importing Package: ${packagePath}
-${unityPath} -projectPath ${projectPath} -quit -batchmode -importPackage ${packagePath} -logFile ${editorLogPath}
