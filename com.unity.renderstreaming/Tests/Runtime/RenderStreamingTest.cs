b5df302eacf7dbafeb9517b2267ffb2809a94fe1
kazuki matsumoto <karasusan@gmail.com>
2021-02-08 16:48:55 +0900

    Add Test

--- com.unity.renderstreaming/Tests/Runtime/RenderStreamingTest.cs
+++ com.unity.renderstreaming/Tests/Runtime/RenderStreamingTest.cs
@@ -1,17 +1,76 @@
+using System;
+using System.Collections;
+using System.Reflection;
 using NUnit.Framework;
+using Unity.RenderStreaming.RuntimeTest.Signaling;
+using Unity.RenderStreaming.Signaling;
 using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
 
 namespace Unity.RenderStreaming.RuntimeTest
 {
+    static class RenderStreamingExtension
+    {
+        private static Type type;
+        private static FieldInfo fieldRunOnAwake;
+
+        static RenderStreamingExtension()
+        {
+            type = typeof(RenderStreaming);
+            fieldRunOnAwake = type.GetField("runOnAwake",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        //public static void SetRunOnAwake(this RenderStreaming obj, bool value)
+        //{
+        //    fieldRunOnAwake.SetValue(obj, value);
+        //}
+    }
+
     class RenderStreamingTest
     {
-        [Test]
-        [Ignore("This test occurs a crash")]
-        public void Test()
+        RenderStreaming component;
+
+        [SetUp]
+        public void SetUp()
         {
             GameObject obj = new GameObject();
-            obj.AddComponent<RenderStreaming>();
-            Object.DestroyImmediate(obj);
+            obj.SetActive(false);
+            component = obj.AddComponent<RenderStreaming>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(component.gameObject);
+        }
+
+        [Test]
+        public void DoNothing()
+        {
+        }
+
+        [Test]
+        public void Run()
+        {
+            ISignaling mock = new MockSignaling();
+            component.runOnAwake = false;
+            component.gameObject.SetActive(true);
+            component.Run(signaling:mock);
+        }
+
+        [UnityTest]
+        public IEnumerator RunAgain()
+        {
+            ISignaling mock = new MockSignaling();
+            component.runOnAwake = false;
+            component.gameObject.SetActive(true);
+            component.Run(signaling: mock);
+            yield return 0;
+            component.Stop();
+            yield return 0;
+            component.Run(signaling: mock);
         }
     }
 }
