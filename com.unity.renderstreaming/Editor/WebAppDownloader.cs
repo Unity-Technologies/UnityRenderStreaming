0dabbb208969a741612271ef1ba6ffdf4b709178
kazuki matsumoto <karasusan@gmail.com>
2021-03-03 08:56:55 +0900

    [skip ci] fix promotion test

--- com.unity.renderstreaming/Editor/WebAppDownloader.cs
+++ com.unity.renderstreaming/Editor/WebAppDownloader.cs
@@ -7,7 +7,7 @@ namespace Unity.RenderStreaming.Editor
     public static class WebAppDownloader
     {
         const string URLRoot = "https://github.com/Unity-Technologies/UnityRenderStreaming";
-        const string LatestKnownVersion = "3.0.0-preview";
+        const string LatestKnownVersion = "3.0.0-preview.1";
 
         // TODO::fix release process of webserver runtime.
         const string FileNameWebAppForMac = "webserver_mac";
