248db6b3a87e9e69f6371c133094489ab81d8bc6
Takashi Kannan <26959415+kannan-xiao4@users.noreply.github.com>
2021-03-02 18:57:15 +0900

    feat: camera streamer use camera component target texture first (#432)
    * camera streamer use cameracompornet target texture first
    
    * update depth inspecter view

--- com.unity.renderstreaming/Runtime/Scripts/CameraStreamer.cs
+++ com.unity.renderstreaming/Runtime/Scripts/CameraStreamer.cs
@@ -1,17 +1,17 @@
 using Unity.WebRTC;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 namespace Unity.RenderStreaming
 {
     [RequireComponent(typeof(Camera))]
     public class CameraStreamer : VideoStreamBase
     {
-        [SerializeField] private RenderTextureDepth depth;
-        [SerializeField, Tooltip("This property is needed to choose from 1,2,4 or 8")]
-        private int antiAliasing = 1; //ToDO(kannan):using enum
+        [SerializeField] private int depth = 0;
+        [SerializeField] private int antiAliasing = 1;
 
 
-        protected Camera m_camera;
+        private Camera m_camera;
         public override Texture SendTexture => m_camera.targetTexture;
 
         protected virtual void Awake()
@@ -21,14 +21,37 @@ namespace Unity.RenderStreaming
 
         protected override MediaStreamTrack CreateTrack()
         {
-            int depthValue = (int)depth;
-            var format = WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
-            var rt = new RenderTexture(streamingSize.x, streamingSize.y, depthValue, format)
+            RenderTexture rt;
+            if (m_camera.targetTexture != null)
             {
-                antiAliasing = antiAliasing
-            };
-            rt.Create();
-            m_camera.targetTexture = rt;
+                rt = m_camera.targetTexture;
+                RenderTextureFormat supportFormat = WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
+                GraphicsFormat graphicsFormat = GraphicsFormatUtility.GetGraphicsFormat(supportFormat, RenderTextureReadWrite.Default);
+                GraphicsFormat compatibleFormat = SystemInfo.GetCompatibleFormat(graphicsFormat, FormatUsage.Render);
+                GraphicsFormat format = graphicsFormat == compatibleFormat ? graphicsFormat : compatibleFormat;
+
+                if (rt.graphicsFormat != format)
+                {
+                    Debug.LogWarning(
+                        $"This color format:{rt.graphicsFormat} not support in unity.webrtc. Change to supported color format:{format}.");
+                    rt.Release();
+                    rt.graphicsFormat = format;
+                    rt.Create();
+                }
+
+                m_camera.targetTexture = rt;
+            }
+            else
+            {
+                RenderTextureFormat format = WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
+                rt = new RenderTexture(streamingSize.x, streamingSize.y, depth, format)
+                {
+                    antiAliasing = antiAliasing
+                };
+                rt.Create();
+                m_camera.targetTexture = rt;
+            }
+
             return new VideoStreamTrack(m_camera.name, rt);
         }
     }
