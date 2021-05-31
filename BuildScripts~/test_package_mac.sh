c037a6e5a114238bb9c60d28ae01c230c04957ff
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2021-05-31 10:07:57 +0900

    chore: Fix scripts for testing macos standalone on CI (#482)
    * [skip ci] fix macos standalone test on CI
    
    * [skip ci] fix
    
    * [skip ci] fix
    
    * [skip ci] fix
    
    * [skip ci] fix
    
    * [skip ci] fix bug
    
    * fix bugs

--- BuildScripts~/test_package_mac.sh
+++ BuildScripts~/test_package_mac.sh
@@ -22,31 +22,47 @@ export IDENTITY=~/.ssh/id_rsa_macmini
 brew install gettext
 
 # render template
-envsubst '                                    \
-  $WEBAPP_DIR                                 \
-  $WEBAPP_NAME                                \
-  $SCRIPTING_BACKEND                          \
-  $EXTRA_UTR_ARG                              \
-  $TEST_PROJECT_DIR                           \
-  $TEST_TARGET                                \
-  $TEST_PLATFORM                              \
-  $EDITOR_VERSION'                            \
-  < ${TEMPLATE_FILE}                          \
+envsubst ' \
+  $BOKKEN_DEVICE_IP \
+  $WEBAPP_DIR \
+  $WEBAPP_NAME \
+  $SCRIPTING_BACKEND \
+  $EXTRA_UTR_ARG \
+  $PLAYER_LOAD_PATH \
+  $TEST_PROJECT_DIR \
+  $TEST_TARGET \
+  $TEST_PLATFORM \
+  $EDITOR_VERSION' \
+  < ${TEMPLATE_FILE} \
   > ~/remote.sh
 chmod +x ~/remote.sh
-
-# copy package to remote machine
-scp -i ${IDENTITY} -r ${YAMATO_SOURCE_DIR} bokken@${BOKKEN_DEVICE_IP}:~/${PACKAGE_DIR}
 
 # copy shell script to remote machine
 scp -i ${IDENTITY} -r ~/remote.sh bokken@${BOKKEN_DEVICE_IP}:~/remote.sh
 
+if [ ${TEST_PLATFORM} = "standalone" ]
+then
+  # copy build player to remote machine
+  # note: need to copy webserver.exe to the remote machine
+  scp -i ${IDENTITY} -r build bokken@${BOKKEN_DEVICE_IP}:~/
+fi
+
+# copy package to remote machine
+scp -i ${IDENTITY} -r ${YAMATO_SOURCE_DIR} bokken@${BOKKEN_DEVICE_IP}:~/${PACKAGE_DIR}  
+
+set +e
+
 # run remote.sh on the remote machine
 ssh -i ${IDENTITY} bokken@${BOKKEN_DEVICE_IP} ~/remote.sh
 result=$?
+
+set -e
+
+# copy artifacts from the remote machine
+mkdir -p ${TEST_RESULT_DIR}
+scp -i ${IDENTITY} -r bokken@${BOKKEN_DEVICE_IP}:~/test-results ${TEST_RESULT_DIR}
+
+# return ssh commend results 
 if [ $result -ne 0 ]; then
   exit $result
 fi
-
-mkdir -p ${TEST_RESULT_DIR}
-scp -i ${IDENTITY} -r bokken@${BOKKEN_DEVICE_IP}:~/test-results ${TEST_RESULT_DIR}
