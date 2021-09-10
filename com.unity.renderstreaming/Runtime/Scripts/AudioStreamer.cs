11a5cfeb1254331ecb8367386ec631408da45458
kazuki <karasusan@gmail.com>
2021-09-10 11:06:51 +0900

    fix audio streamer

--- com.unity.renderstreaming/Runtime/Scripts/AudioStreamer.cs
+++ com.unity.renderstreaming/Runtime/Scripts/AudioStreamer.cs
@@ -1,20 +1,44 @@
-using System;
 using Unity.WebRTC;
 using UnityEngine;
 
 namespace Unity.RenderStreaming
 {
-    [RequireComponent(typeof(AudioSource))]
+    /// <summary>
+    /// Attach AudioListerner or AudioSource
+    /// </summary>
     public class AudioStreamer : AudioStreamBase
     {
         public AudioSource audioSource;
+        public int sampleRate = 48000;
 
         private AudioStreamTrack track;
 
         protected override MediaStreamTrack CreateTrack()
         {
-            track = new AudioStreamTrack(audioSource);
+            track = new AudioStreamTrack();
             return track;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            track?.Dispose();
+        }
+
+        protected virtual void OnEnable()
+        {
+            if(track != null)
+                track.Enabled = true;
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (track != null)
+                track.Enabled = false;
+        }
+
+        private void OnAudioFilterRead(float[] data, int channels)
+        {
+            track?.SetData(data, channels, sampleRate);
         }
     }
 }
