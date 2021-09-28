280b9ce9a22d10de8e1b187f0bef1aa83946b3e8
Samuel Tranchet <samuel.tranchet@unity3d.com>
2021-09-28 11:21:35 +0200

    Allow to list ISignaling classes in all assemblies of a unity project, not only in this package
    + Revert "Small update in Furioos enable streaming JSON message" to remain compatible with Furioos 1.4.3.142 (production version)

--- com.unity.renderstreaming/Runtime/Scripts/Signaling/FurioosSignaling.cs
+++ com.unity.renderstreaming/Runtime/Scripts/Signaling/FurioosSignaling.cs
@@ -171,7 +171,7 @@ namespace Unity.RenderStreaming.Signaling
                         if (msg.status == "SUCCESS")
                         {
                             Debug.Log("Signaling: Slot signed in.");
-                            this.WSSend("{\"type\":\"furioos\",\"task\":\"enableStreaming\",\"streamType\":\"RenderStreaming\",\"streamProtocols\":[\"WebRTC\"],\"controlsTypes\":[\"RenderStreaming\"]}");
+                            this.WSSend("{\"type\":\"furioos\",\"task\":\"enableStreaming\",\"streamTypes\":\"WebRTC\",\"controlType\":\"RenderStreaming\"}");
 
                             OnSignedIn?.Invoke(this);
                         }
