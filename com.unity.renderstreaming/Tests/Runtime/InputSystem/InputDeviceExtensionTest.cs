4c1e166740647bb484a51614369cb8efcea19456
kazuki matsumoto <karasusan@gmail.com>
2021-10-27 12:37:34 +0900

    fix issues

---
+++ com.unity.renderstreaming/Tests/Runtime/InputSystem/InputDeviceExtensionTest.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using Assert = NUnit.Framework.Assert;
+
+namespace Unity.RenderStreaming.RuntimeTest
+{
+    class InputDeviceExtensionTest
+    {
+        [Test]
+        public void SetDescription()
+        {
+            var device = InputSystem.AddDevice<Mouse>();
+            InputDeviceDescription empty;
+            InputDeviceDescription origin = device.description;
+            device.SetDescription(empty);
+            Assert.That(device.description, Is.EqualTo(empty));
+            device.SetDescription(origin);
+            Assert.That(device.description, Is.EqualTo(origin));
+        }
+        [Test]
+        public void SetParticipantId()
+        {
+            var device = InputSystem.AddDevice<Mouse>();
+            int participantId = 0;
+            int origin = device.GetParticipantId();
+            device.SetParticipantId(participantId);
+            Assert.That(device.GetParticipantId(), Is.EqualTo(participantId));
+            device.SetParticipantId(origin);
+            Assert.That(device.GetParticipantId(), Is.EqualTo(origin));
+        }
+        [Test]
+        public void SetDeviceFlags()
+        {
+            var device = InputSystem.AddDevice<Mouse>();
+            int deviceFlags = 0;
+            int origin = device.GetDeviceFlags();
+            device.SetDeviceFlags(deviceFlags);
+            Assert.That(device.GetDeviceFlags(), Is.EqualTo(deviceFlags));
+            device.SetDeviceFlags(origin);
+            Assert.That(device.GetDeviceFlags(), Is.EqualTo(origin));
+        }
+    }
+}
--- com.unity.renderstreaming/Tests/Runtime/InputSystem/InputRemotingTest.cs
+++ com.unity.renderstreaming/Tests/Runtime/InputSystem/InputRemotingTest.cs
@@ -34,43 +34,6 @@ namespace Unity.RenderStreaming.RuntimeTest
         }
     }
 
-    class InputDeviceExtensionTest
-    {
-        [Test]
-        public void SetDescription()
-        {
-            var device = InputSystem.AddDevice<Mouse>();
-            InputDeviceDescription empty;
-            InputDeviceDescription origin = device.description;
-            device.SetDescription(empty);
-            Assert.That(device.description, Is.EqualTo(empty));
-            device.SetDescription(origin);
-            Assert.That(device.description, Is.EqualTo(origin));
-        }
-        [Test]
-        public void SetParticipantId()
-        {
-            var device = InputSystem.AddDevice<Mouse>();
-            int participantId = 0;
-            int origin = device.GetParticipantId();
-            device.SetParticipantId(participantId);
-            Assert.That(device.GetParticipantId(), Is.EqualTo(participantId));
-            device.SetParticipantId(origin);
-            Assert.That(device.GetParticipantId(), Is.EqualTo(origin));
-        }
-        [Test]
-        public void SetDeviceFlags()
-        {
-            var device = InputSystem.AddDevice<Mouse>();
-            int deviceFlags = 0;
-            int origin = device.GetDeviceFlags();
-            device.SetDeviceFlags(deviceFlags);
-            Assert.That(device.GetDeviceFlags(), Is.EqualTo(deviceFlags));
-            device.SetDeviceFlags(origin);
-            Assert.That(device.GetDeviceFlags(), Is.EqualTo(origin));
-        }
-    }
-
     /// todo(kazuki):workaround
     class InputRemotingTest
     {
