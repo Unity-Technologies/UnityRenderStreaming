d0e2cbde86496b37a3c1b7d7257f22e569fb781f
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-10-27 12:24:02 +0900

    remove videolayer buttons

--- WebApp/client/public/videoplayer/js/main.js
+++ WebApp/client/public/videoplayer/js/main.js
@@ -1,5 +1,4 @@
 import { VideoPlayer } from "./video-player.js";
-import { sendClickEvent } from "../../js/register-events.js";
 import { getServerConfig } from "../../js/config.js";
 
 setup();
@@ -60,33 +59,6 @@ function onClickPlayButton() {
 
 
   setupVideoPlayer(elementVideo).then(value => videoPlayer = value);
-
-  // add blue button
-  const elementBlueButton = document.createElement('button');
-  elementBlueButton.id = "blueButton";
-  elementBlueButton.innerHTML = "Light on";
-  playerDiv.appendChild(elementBlueButton);
-  elementBlueButton.addEventListener("click", function () {
-    sendClickEvent(videoPlayer, 1);
-  });
-
-  // add green button
-  const elementGreenButton = document.createElement('button');
-  elementGreenButton.id = "greenButton";
-  elementGreenButton.innerHTML = "Light off";
-  playerDiv.appendChild(elementGreenButton);
-  elementGreenButton.addEventListener("click", function () {
-    sendClickEvent(videoPlayer, 2);
-  });
-
-  // add orange button
-  const elementOrangeButton = document.createElement('button');
-  elementOrangeButton.id = "orangeButton";
-  elementOrangeButton.innerHTML = "Play audio";
-  playerDiv.appendChild(elementOrangeButton);
-  elementOrangeButton.addEventListener("click", function () {
-    sendClickEvent(videoPlayer, 3);
-  });
 
   // add fullscreen button
   const elementFullscreenButton = document.createElement('img');
