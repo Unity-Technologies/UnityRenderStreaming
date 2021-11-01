62cb91603e1c009947a33fbaf29afa4d9f2359d9
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-11-01 18:16:34 +0900

    use custom event system on broadcast

---
+++ com.unity.renderstreaming/Samples~/Example/Broadcast/CustomEventSystem.cs
@@ -0,0 +1,32 @@
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class CustomEventSystem : EventSystem
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        unsafe
+        {
+            InputSystem.onDeviceCommand += InputSystemOnDeviceCommand;
+        }
+    }
+
+    private static unsafe long? InputSystemOnDeviceCommand(InputDevice device, InputDeviceCommand* command)
+    {
+        if (command->type != QueryCanRunInBackground.Type)
+        {
+            // return null is skip this evaluation
+            return null;
+        }
+
+        ((QueryCanRunInBackground*)command)->canRunInBackground = true;
+        return InputDeviceCommand.GenericSuccess;
+    }
+
+    protected override void OnApplicationFocus(bool hasFocus)
+    {
+        //Do not change focus flag on eventsystem
+    }
+}
