539b0fcd0536943d1b17e72025d2f47f3060a2b0
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2021-11-09 16:54:55 +0900

    fix: Fixed ScreenStreamSender inspector UI (#572)

--- com.unity.renderstreaming/Editor/CameraStreamSenderEditor.cs
+++ com.unity.renderstreaming/Editor/CameraStreamSenderEditor.cs
@@ -1,48 +1,17 @@
+#if UNITY_EDITOR
 using UnityEditor;
-using UnityEngine;
 
 namespace Unity.RenderStreaming.Editor
 {
     [CustomEditor(typeof(CameraStreamSender))]
     public class CameraStreamSenderEditor : UnityEditor.Editor
     {
-        readonly GUIContent[] renderTextureAntiAliasing = new GUIContent[4]
-        {
-            EditorGUIUtility.TrTextContent("None"),
-            EditorGUIUtility.TrTextContent("2 samples"),
-            EditorGUIUtility.TrTextContent("4 samples"),
-            EditorGUIUtility.TrTextContent("8 samples")
-        };
-
-        readonly int[] renderTextureAntiAliasingValues = new int[4] {1, 2, 4, 8};
-
-        readonly GUIContent antiAliasing =
-            EditorGUIUtility.TrTextContent("Anti-aliasing", "Number of anti-aliasing samples.");
-
-        readonly GUIContent[] renderTextureDepthBuffer = new GUIContent[3]
-        {
-            EditorGUIUtility.TrTextContent("No depth buffer"),
-            EditorGUIUtility.TrTextContent("At least 16 bits depth (no stencil)"),
-            EditorGUIUtility.TrTextContent("At least 24 bits depth (with stencil)")
-        };
-
-        readonly int[] renderTextureDepthBufferValues = new int[3] {0, 16, 24};
-
-        readonly GUIContent depthBuffer = EditorGUIUtility.TrTextContent("Depth Buffer", "Format of the depth buffer.");
-
         public override void OnInspectorGUI()
         {
-            serializedObject.Update();
-
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("streamingSize"));
-            var antiAliasingProperty = serializedObject.FindProperty("antiAliasing");
-            EditorGUILayout.IntPopup(antiAliasingProperty, renderTextureAntiAliasing, renderTextureAntiAliasingValues, antiAliasing);
-            var depthBufferProperty = serializedObject.FindProperty("depth");
-            EditorGUILayout.IntPopup(depthBufferProperty, renderTextureDepthBuffer, renderTextureDepthBufferValues, depthBuffer);
-
-            serializedObject.ApplyModifiedProperties();
+            base.OnInspectorGUI();
 
             EditorGUILayout.HelpBox("If TargetTexture is attached on Camera, use that RenderTexture setting first.", MessageType.Info);
         }
     }
 }
+#endif
