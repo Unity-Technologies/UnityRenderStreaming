ffc23aa5c19d8573046553f9966e8ed81f1c4adf
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-11-17 10:17:44 +0900

    add get transceivers method

--- WebApp/client/public/js/peer.js
+++ WebApp/client/public/js/peer.js
@@ -83,22 +83,31 @@ export default class Peer extends EventTarget {
     }
   }
 
+  getTransceivers(connectionId) {
+    if (this.connectionId != connectionId) {
+      return null;
+    }
+
+    return this.pc.getTransceivers();
+  }
+
   addTrack(connectionId, track) {
     if (this.connectionId != connectionId) {
-      return;
+      return null;
     }
 
     const sender = this.pc.addTrack(track);
     const transceiver = this.pc.getTransceivers().find((t) => t.sender == sender);
     transceiver.direction = "sendonly";
+    return sender;
   }
 
   addTransceiver(connectionId, trackOrKind, init) {
     if (this.connectionId != connectionId) {
-      return;
+      return null;
     }
 
-    this.pc.addTransceiver(trackOrKind, init);
+    return this.pc.addTransceiver(trackOrKind, init);
   }
 
   createDataChannel(connectionId, label) {
