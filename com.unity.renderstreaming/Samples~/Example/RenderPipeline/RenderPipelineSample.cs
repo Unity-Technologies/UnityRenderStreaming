6c0d4fc7d19112536e98bf4bf2908d71ebcaadfc
Takashi KANNAN <takashi.kannan@unity3d.com>
2021-11-19 11:16:53 +0900

    fix renderpipeline sample script

--- com.unity.renderstreaming/Samples~/Example/RenderPipeline/RenderPipelineSample.cs
+++ com.unity.renderstreaming/Samples~/Example/RenderPipeline/RenderPipelineSample.cs
@@ -1,31 +1,20 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Unity.RenderStreaming.Samples
 {
     class RenderPipelineSample : MonoBehaviour
     {
         [SerializeField] RenderStreaming renderStreaming;
-        [SerializeField] Dropdown dropdownCamera;
-        [SerializeField] Transform[] cameras;
-        [SerializeField] CopyTransform copyTransform;
 
         // Start is called before the first frame update
         void Start()
         {
-            dropdownCamera.onValueChanged.AddListener(OnChangeCamera);
-
             if (!renderStreaming.runOnAwake)
             {
                 renderStreaming.Run(
                     hardwareEncoder: RenderStreamingSettings.EnableHWCodec,
                     signaling: RenderStreamingSettings.Signaling);
             }
-        }
-
-        void OnChangeCamera(int value)
-        {
-            copyTransform.SetOrigin(cameras[value]);
         }
     }
 }
