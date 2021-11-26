3c365ccf5805779752485f7d8c3993a866a3c66e
Takashi Kannan <26959415+kannan-xiao4@users.noreply.github.com>
2021-11-26 14:13:57 +0900

    fix: Avoid crash if using specified Screen resolution (#594)
    * fixed streming size on screenstreamsender
    
    * direct into screencapture if same resolution screen and streaming
    
    * destory sendtexture onstoppedstream
    
    * capture and copy texture if streaming video
    
    * fix streaming flag has some connection

--- com.unity.renderstreaming/Runtime/Scripts/ScreenStreamSender.cs
+++ com.unity.renderstreaming/Runtime/Scripts/ScreenStreamSender.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.WebRTC;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
@@ -6,52 +8,112 @@ using UnityEngine.Experimental.Rendering;
 namespace Unity.RenderStreaming
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public class ScreenStreamSender : VideoStreamSender
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [SerializeField, RenderTextureDepthBuffer]
         private int depth = 0;
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [SerializeField, RenderTextureAntiAliasing]
         private int antiAliasing = 1;
 
         public override Texture SendTexture => m_sendTexture;
         private RenderTexture m_sendTexture;
+        private RenderTexture m_screenTexture;
+        private HashSet<string> connections = new HashSet<string>();
 
         protected virtual void Awake()
         {
-            RenderTextureFormat supportFormat =
-                WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
-            GraphicsFormat graphicsFormat =
-                GraphicsFormatUtility.GetGraphicsFormat(supportFormat, RenderTextureReadWrite.Default);
-            GraphicsFormat compatibleFormat = SystemInfo.GetCompatibleFormat(graphicsFormat, FormatUsage.Render);
-            GraphicsFormat format = graphicsFormat == compatibleFormat ? graphicsFormat : compatibleFormat;
-
-            m_sendTexture =
+            var format = WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
+            m_screenTexture =
                 new RenderTexture(Screen.width, Screen.height, depth, format) { antiAliasing = antiAliasing };
+            m_screenTexture.Create();
+
+            StartCoroutine(RecordScreenFrame());
+
+            OnStartedStream += id => connections.Add(id);
+            OnStoppedStream += id => connections.Remove(id);
+        }
+
+        protected void OnDestroy()
+        {
+            if (m_sendTexture != null)
+            {
+                DestroyImmediate(m_sendTexture);
+                m_sendTexture = null;
+            }
+
+            if (m_screenTexture != null)
+            {
+                DestroyImmediate(m_screenTexture);
+                m_screenTexture = null;
+            }
         }
 
         protected override MediaStreamTrack CreateTrack()
         {
-            return new VideoStreamTrack(m_sendTexture.GetNativeTexturePtr(), m_sendTexture.width, m_sendTexture.height, m_sendTexture.graphicsFormat);
+            RenderTexture rt;
+            if (m_sendTexture != null)
+            {
+                rt = m_sendTexture;
+                RenderTextureFormat supportFormat =
+                    WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
+                GraphicsFormat graphicsFormat =
+                    GraphicsFormatUtility.GetGraphicsFormat(supportFormat, RenderTextureReadWrite.Default);
+                GraphicsFormat compatibleFormat = SystemInfo.GetCompatibleFormat(graphicsFormat, FormatUsage.Render);
+                GraphicsFormat format = graphicsFormat == compatibleFormat ? graphicsFormat : compatibleFormat;
+
+                if (rt.graphicsFormat != format)
+                {
+                    Debug.LogWarning(
+                        $"This color format:{rt.graphicsFormat} not support in unity.webrtc. Change to supported color format:{format}.");
+                    rt.Release();
+                    rt.graphicsFormat = format;
+                    rt.Create();
+                }
+
+                m_sendTexture = rt;
+            }
+            else
+            {
+                RenderTextureFormat format =
+                    WebRTC.WebRTC.GetSupportedRenderTextureFormat(SystemInfo.graphicsDeviceType);
+                rt = new RenderTexture(streamingSize.x, streamingSize.y, depth, format) { antiAliasing = antiAliasing };
+                rt.Create();
+                m_sendTexture = rt;
+            }
+
+            return new VideoStreamTrack(rt.GetNativeTexturePtr(), rt.width, rt.height, rt.graphicsFormat);
         }
 
-        protected void LateUpdate()
+        IEnumerator RecordScreenFrame()
         {
-            StartCoroutine(RecordFrame());
-        }
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
 
-        IEnumerator RecordFrame()
-        {
-            yield return new WaitForEndOfFrame();
-            ScreenCapture.CaptureScreenshotIntoRenderTexture(m_sendTexture);
+                if (!connections.Any() || m_sendTexture == null || !m_sendTexture.IsCreated())
+                {
+                    continue;
+                }
+
+                if (m_screenTexture.width == m_sendTexture.width && m_screenTexture.height == m_sendTexture.height)
+                {
+                    ScreenCapture.CaptureScreenshotIntoRenderTexture(m_sendTexture);
+                }
+                else
+                {
+                    ScreenCapture.CaptureScreenshotIntoRenderTexture(m_screenTexture);
+                    Graphics.Blit(m_screenTexture, m_sendTexture);
+                }
+            }
         }
     }
 }
