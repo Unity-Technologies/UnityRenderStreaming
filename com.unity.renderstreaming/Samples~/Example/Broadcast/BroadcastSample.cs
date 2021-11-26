58773ef1520912a253aea21db41a22530fd355bc
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2021-11-26 10:16:49 +0900

    fix: Fixed InputActionAssets for AR Foundation sample (#593)

--- com.unity.renderstreaming/Samples~/Example/Broadcast/BroadcastSample.cs
+++ com.unity.renderstreaming/Samples~/Example/Broadcast/BroadcastSample.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.XR;
 
 namespace Unity.RenderStreaming.Samples
 {
+    using InputSystem = UnityEngine.InputSystem.InputSystem;
+
     class BroadcastSample : MonoBehaviour
     {
         [SerializeField] RenderStreaming renderStreaming;
+
+        private void Awake()
+        {
+#if URS_USE_AR_FOUNDATION
+            InputSystem.RegisterLayout<UnityEngine.XR.ARSubsystems.HandheldARInputDevice>(
+                matches: new InputDeviceMatcher()
+                    .WithInterface(XRUtilities.InterfaceMatchAnyVersion)
+                );
+#endif
+        }
 
         // Start is called before the first frame update
         void Start()
