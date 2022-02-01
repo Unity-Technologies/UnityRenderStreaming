e45350302cf5e23af9979eb5cf39f7670dcaee1d
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-02-01 10:16:37 +0900

    fix compile error

--- com.unity.renderstreaming/Runtime/Scripts/VideoStreamReceiver.cs
+++ com.unity.renderstreaming/Runtime/Scripts/VideoStreamReceiver.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 namespace Unity.RenderStreaming
 {
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public class VideoStreamReceiver : StreamReceiverBase
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="receiveTexture"></param>
         public delegate void OnUpdateReceiveTextureHandler(Texture receiveTexture);
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public OnUpdateReceiveTextureHandler OnUpdateReceiveTexture;
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public override TrackKind Kind { get { return TrackKind.Video; } }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         [SerializeField] private Vector2Int streamingSize = new Vector2Int(1280, 720);
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public Texture ReceiveTexture => m_receiveTexture;
 
@@ -44,9 +44,14 @@ namespace Unity.RenderStreaming
 
         private void StartedStream(string connectionId)
         {
-            var videoTrack = Track as VideoStreamTrack;
-            m_receiveTexture = videoTrack?.InitializeReceiver(streamingSize.x, streamingSize.y);
-            OnUpdateReceiveTexture?.Invoke(m_receiveTexture);
+            if (Track is VideoStreamTrack videoTrack)
+            {
+                videoTrack.OnVideoReceived += texture =>
+                {
+                    m_receiveTexture = texture;
+                    OnUpdateReceiveTexture?.Invoke(m_receiveTexture);
+                };
+            }
         }
 
         private void StoppedStream(string connectionId)
