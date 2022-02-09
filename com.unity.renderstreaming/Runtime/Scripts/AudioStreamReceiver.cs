8abf7105bd668fa6d48c7675b0acd55ab80ff22a
kazuki <karasusan@gmail.com>
2022-02-09 10:50:53 +0900

    do not run callback when stopping streaming audio

--- com.unity.renderstreaming/Runtime/Scripts/AudioStreamReceiver.cs
+++ com.unity.renderstreaming/Runtime/Scripts/AudioStreamReceiver.cs
@@ -58,7 +58,6 @@ namespace Unity.RenderStreaming
         private void StoppedStream(string connectionId)
         {
             m_source = null;
-            OnUpdateReceiveAudioSource?.Invoke(null);
         }
     }
 }
