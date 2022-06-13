227052b17158e866e6ef3ed999371ab514d611a0
kazuki <karasusan@gmail.com>
2022-06-13 14:34:07 +0900

    ignore lint error

--- WebApp/client/test/resizeobservermock.js
+++ WebApp/client/test/resizeobservermock.js
@@ -1,6 +1,8 @@
 // mock class
 
+/* eslint-disable no-unused-vars */
 let instanceResize = null;
+/* eslint-disable no-unused-vars */
 let callbackResize = null;
 
 export default class ResizeObserverMock {
@@ -9,6 +11,8 @@ export default class ResizeObserverMock {
     callbackResize = callback;
   }
   disconnect() { }
+  /* eslint-disable no-unused-vars */
   observe(target, options) { }
+  /* eslint-disable no-unused-vars */
   unobserve(target) { }
 }
