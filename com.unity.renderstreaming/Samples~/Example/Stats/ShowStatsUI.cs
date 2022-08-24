e060eeee49ef88565ce119ddefb00387c00d2a00
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-08-24 11:02:59 +0900

    add stats ui

---
+++ com.unity.renderstreaming/Samples~/Example/Stats/ShowStatsUI.cs
@@ -0,0 +1,197 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unity.WebRTC;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.RenderStreaming.Samples
+{
+    public class ShowStatsUI : MonoBehaviour
+    {
+        [SerializeField] private Toggle toggle;
+        [SerializeField] private GameObject scrollView;
+        [SerializeField] private RectTransform displayParent;
+        [SerializeField] private Text baseText;
+
+        [SerializeField] private List<StreamSenderBase> senderBaseList;
+        [SerializeField] private List<StreamReceiverBase> receiverBaseList;
+
+        private void Awake()
+        {
+            toggle.onValueChanged.AddListener(SwitchToggle);
+        }
+
+        private void SwitchToggle(bool isOn)
+        {
+            scrollView.SetActive(isOn);
+        }
+
+        private void Start()
+        {
+            StartCoroutine(CollectStats());
+        }
+
+        private void OnDestroy()
+        {
+            lastSenderStats.Clear();
+            lastReceiverStats.Clear();
+        }
+
+        private Dictionary<RTCRtpSender, StatsDisplay> lastSenderStats = new Dictionary<RTCRtpSender, StatsDisplay>();
+        private Dictionary<RTCRtpReceiver, StatsDisplay> lastReceiverStats = new Dictionary<RTCRtpReceiver, StatsDisplay>();
+
+        class StatsDisplay
+        {
+            public Text display;
+            public RTCStatsReport lastReport;
+        }
+
+        private IEnumerator CollectStats()
+        {
+            var waitSec = new WaitForSeconds(1);
+
+            while (true)
+            {
+                yield return waitSec;
+
+                foreach (var transceiver in senderBaseList.SelectMany(x => x.Transceivers.Values))
+                {
+                    var op = transceiver.Sender.GetStats();
+                    yield return op;
+                    if (op.IsError)
+                    {
+                        continue;
+                    }
+
+                    var report = op.Value;
+                    if (lastSenderStats.TryGetValue(transceiver.Sender, out var statsDisplay))
+                    {
+                        var displayString = CreateDisplayString(report, statsDisplay.lastReport);
+                        statsDisplay.display.text = displayString;
+                        statsDisplay.lastReport = report;
+                    }
+                    else
+                    {
+                        var text = Instantiate(baseText, displayParent);
+                        text.text = "";
+                        text.gameObject.SetActive(true);
+                        lastSenderStats[transceiver.Sender] = new StatsDisplay {display = text, lastReport = report};
+                    }
+                }
+
+                foreach (var transceiver in receiverBaseList.Select(x => x.Transceiver))
+                {
+                    var op = transceiver.Receiver.GetStats();
+                    yield return op;
+
+                    var report = op.Value;
+                    if (lastReceiverStats.TryGetValue(transceiver.Receiver, out var statsDisplay))
+                    {
+                        var displayString = CreateDisplayString(report, statsDisplay.lastReport);
+                        statsDisplay.display.text = displayString;
+                        statsDisplay.lastReport = report;
+                    }
+                    else
+                    {
+                        var text = Instantiate(baseText, displayParent);
+                        text.text = "";
+                        text.gameObject.SetActive(true);
+                        lastReceiverStats[transceiver.Receiver] = new StatsDisplay {display = text, lastReport = report};
+                    }
+                }
+
+
+                if (lastSenderStats.Any() || lastReceiverStats.Any())
+                {
+                    baseText.gameObject.SetActive(false);
+                    // Correct scroll view contents height
+                    var size = displayParent.sizeDelta;
+                    size.y = baseText.GetComponent<RectTransform>().sizeDelta.y * (displayParent.childCount - 1) + 100;
+                    displayParent.sizeDelta = size;
+                }
+            }
+        }
+
+        private static string CreateDisplayString(RTCStatsReport report, RTCStatsReport lastReport)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var stats in report.Stats.Values)
+            {
+                if (stats is RTCInboundRTPStreamStats inboundStats)
+                {
+                    builder.AppendLine($"{nameof(RTCInboundRTPStreamStats)}");
+                    if (inboundStats.codecId != null &&
+                        report.TryGetValue(inboundStats.codecId, out var stats2) &&
+                        stats2 is RTCCodecStats codecStats)
+                    {
+                        builder.AppendLine(
+                            $"Codec: {codecStats.mimeType} {codecStats.sdpFmtpLine}, payloadType={codecStats.payloadType}.");
+                    }
+
+                    if (inboundStats.kind == "video")
+                    {
+                        builder.AppendLine($"Decoder: {inboundStats.decoderImplementation}.");
+                        builder.AppendLine($"Resolution: {inboundStats.frameWidth}x{inboundStats.frameHeight}");
+                        builder.AppendLine($"Framerate: {inboundStats.framesPerSecond}");
+                    }
+
+                    if (lastReport.Get(inboundStats.Id) is RTCInboundRTPStreamStats lastInboundStats)
+                    {
+                        var duration = (double)(inboundStats.Timestamp - lastInboundStats.Timestamp) / 1000000;
+                        var bitrate = (ulong)(8 * (inboundStats.bytesReceived - lastInboundStats.bytesReceived) / duration);
+                        builder.AppendLine($"Bitrate: {bitrate}");
+                    }
+                }
+                else if (stats is RTCOutboundRTPStreamStats outboundStats)
+                {
+                    builder.AppendLine($"{nameof(RTCOutboundRTPStreamStats)}");
+                    if (outboundStats.codecId != null &&
+                        report.TryGetValue(outboundStats.codecId, out var stats4) &&
+                        stats4 is RTCCodecStats codecStats)
+                    {
+                        builder.AppendLine(
+                            $"Codec: {codecStats.mimeType} {codecStats.sdpFmtpLine}, payloadType={codecStats.payloadType}.");
+                    }
+
+                    if (outboundStats.kind == "video")
+                    {
+                        builder.AppendLine($"Encoder: {outboundStats.encoderImplementation}.");
+                        builder.AppendLine($"Resolution: {outboundStats.frameWidth}x{outboundStats.frameHeight}");
+                        builder.AppendLine($"Framerate: {outboundStats.framesPerSecond}");
+                    }
+
+                    if (lastReport.Get(outboundStats.Id) is RTCOutboundRTPStreamStats lastOutBoundStats)
+                    {
+                        var duration = (double)(outboundStats.Timestamp - lastOutBoundStats.Timestamp) / 1000000;
+                        var bitrate = (ulong)(8 * (outboundStats.bytesSent - lastOutBoundStats.bytesSent) / duration);
+                        builder.AppendLine($"Bitrate: {bitrate}");
+                    }
+                }
+            }
+            return builder.ToString();
+        }
+
+        public void AddSenderBase(StreamSenderBase sender)
+        {
+            if (senderBaseList.Contains(sender))
+            {
+                return;
+            }
+
+            senderBaseList.Add(sender);
+        }
+
+        public void AddReceiverBase(StreamReceiverBase receiver)
+        {
+            if (receiverBaseList.Contains(receiver))
+            {
+                return;
+            }
+
+            receiverBaseList.Add(receiver);
+        }
+    }
+}
