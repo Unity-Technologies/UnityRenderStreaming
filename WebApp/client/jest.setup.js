078e1e7ee62e078b9685d2f46caf9593ea391db9
Takashi KANNAN <takashi.kannan@unity3d.com>
2022-09-06 11:49:38 +0900

    fix lint

--- WebApp/client/jest.setup.js
+++ WebApp/client/jest.setup.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-undef */
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
+import { TextEncoder, TextDecoder } from 'util';
 import { PeerConnectionMock, SessionDescriptionMock, IceCandidateMock } from './test/peerconnectionmock';
 import ResizeObserverMock from './test/resizeobservermock';
 
+// note: If set testEnvironment `jest-environment-jsdom`,  below classes are not defined.
+
 if (!window.fetch) {
   window.fetch = fetch;
+}
+
+if (!window.TextEncoder) {
+  window.TextEncoder = TextEncoder;
+}
+
+if (!window.TextDecoder) {
+  window.TextDecoder = TextDecoder;
 }
 
 if (!window.RTCPeerConnection) {
