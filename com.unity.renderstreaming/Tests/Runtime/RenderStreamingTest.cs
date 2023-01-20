ee725d3a2612da3f63425aa5f248ad41a30f4f7b
kazuki <karasusan@gmail.com>
2023-01-20 14:34:24 +0900

    add class

---
+++ com.unity.renderstreaming/Tests/Runtime/RenderStreamingTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using NUnit.Framework;
+using Unity.RenderStreaming.RuntimeTest.Signaling;
+using Unity.RenderStreaming.Signaling;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace Unity.RenderStreaming.RuntimeTest
+{
+    class RenderStreamingTest
+    {
+        // todo
+    }
+}
