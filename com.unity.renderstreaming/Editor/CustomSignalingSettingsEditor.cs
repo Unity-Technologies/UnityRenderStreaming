7e85377444d48c07d8344fd64f9ccc31f34082f8
Takashi KANNAN <takashi.kannan@unity3d.com>
2023-01-24 16:14:06 +0900

    custom signaling settings editor

---
+++ com.unity.renderstreaming/Editor/CustomSignalingSettingsEditor.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.RenderStreaming.Editor
+{
+    public class CustomSignalingSettingsEditor : Attribute
+    {
+        private static readonly TypeCache.TypeCollection customInspectorType =
+            TypeCache.GetTypesWithAttribute<CustomSignalingSettingsEditor>();
+
+        private readonly Type inspectedType;
+
+        public CustomSignalingSettingsEditor(Type inspectedType)
+        {
+            if (inspectedType == null)
+                Debug.LogError("Failed to load CustomEditor inspected type");
+            this.inspectedType = inspectedType;
+        }
+
+        internal static Type FindCustomInspectorTypeByType(Type inspectorType)
+        {
+            foreach (var type in customInspectorType)
+            {
+                foreach (CustomSignalingSettingsEditor custom in
+                         type.GetCustomAttributes(typeof(CustomSignalingSettingsEditor), false))
+                {
+                    if (custom.inspectedType == inspectorType)
+                    {
+                        return type;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
