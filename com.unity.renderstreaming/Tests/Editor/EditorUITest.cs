a61049fd2348ea9438256799e434e4bf691d74e6
kazuki <karasusan@gmail.com>
2023-01-25 17:46:45 +0900

    wip

--- com.unity.renderstreaming/Tests/Editor/EditorUITest.cs
+++ com.unity.renderstreaming/Tests/Editor/EditorUITest.cs
@@ -1,8 +1,6 @@
 using System;
 using NUnit.Framework;
 using Unity.RenderStreaming.Editor;
-using UnityEditor;
-using UnityEngine.UIElements;
 
 namespace Unity.RenderStreaming.EditorTest
 {
@@ -10,46 +8,21 @@ namespace Unity.RenderStreaming.EditorTest
 
     class CustomSignalingSettingsEditorTest
     {
-        class DummySignalingSettings : SignalingSettings
-        {
-            public override Type signalingClass => null;
-        }
-
-        [CustomSignalingSettingsEditor(typeof(DummySignalingSettings))]
-        class DummySignalingSettingsEditor : ISignalingSettingEditor
-        {
-            public string Label => "Mock";
-
-            public VisualElement ExtendInspectorGUI()
-            {
-                return new TextField("Interval");
-            }
-
-            public void SetSignalingSettings(SignalingSettings settings)
-            {
-            }
-        }
-
         [Test]
         public void FindCustomInspectorTypeByType()
         {
             Type type = typeof(WebSocketSignalingSettings);
-            Assert.That(CustomSignalingSettingsEditor.FindCustomInspectorTypeByType(type), Is.EqualTo(typeof(WebSocketSignalingSettingsEditor)));
+            Assert.That(CustomSignalingSettingsEditor.FindInspectorTypeByInspectedType(type), Is.EqualTo(typeof(WebSocketSignalingSettingsEditor)));
 
             type = typeof(HttpSignalingSettings);
-            Assert.That(CustomSignalingSettingsEditor.FindCustomInspectorTypeByType(type), Is.EqualTo(typeof(HttpSignalingSettingsEditor)));
-
-            type = typeof(DummySignalingSettings);
-            Assert.That(CustomSignalingSettingsEditor.FindCustomInspectorTypeByType(type), Is.EqualTo(typeof(DummySignalingSettingsEditor)));
+            Assert.That(CustomSignalingSettingsEditor.FindInspectorTypeByInspectedType(type), Is.EqualTo(typeof(HttpSignalingSettingsEditor)));
         }
 
         [Test]
-        public void GetAllSignalingTypes()
+        public void FindAllInspectorType()
         {
-            var types = TypeCache.GetTypesDerivedFrom<SignalingSettings>();
-            Assert.That(types, Has.Member(typeof(WebSocketSignalingSettings)));
-            Assert.That(types, Has.Member(typeof(HttpSignalingSettings)));
-            Assert.That(types, Has.Member(typeof(DummySignalingSettings)));
+            var types = CustomSignalingSettingsEditor.Labels();
+            Assert.That(types, Is.Not.Empty);
         }
     }
 }
