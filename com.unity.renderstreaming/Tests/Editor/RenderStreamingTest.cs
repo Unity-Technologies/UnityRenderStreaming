bb16b5f50604391d81a620d5232805120ce41c99
Takashi KANNAN <takashi.kannan@unity3d.com>
2023-01-26 12:40:46 +0900

    add setting load test

--- com.unity.renderstreaming/Tests/Editor/RenderStreamingTest.cs
+++ com.unity.renderstreaming/Tests/Editor/RenderStreamingTest.cs
@@ -1,13 +1,27 @@
 using NUnit.Framework;
+using Unity.WebRTC;
 
 namespace Unity.RenderStreaming.EditorTest
 {
     public class RenderStreamingTest
     {
         [Test]
-        public void RenderStreamingSettings()
+        public void SignalingSettings()
         {
-            // todo: check RenderStreaming.GetSignalingSettings method that return correct settings from asset
+            var url = "wss://127.0.0.1:8081";
+            var iceServers = new RTCIceServer[]
+            {
+                new RTCIceServer() {urls = new string[] {"stun:stun.l.google.com:19302"}}
+            };
+            var signalingSettings = new WebSocketSignalingSettings();
+            signalingSettings.urlSignaling = url;
+            signalingSettings.iceServers = iceServers;
+
+            Assert.That(() => RenderStreaming.SetSignalingSettings(signalingSettings), Throws.Nothing);
+
+            var settings = RenderStreaming.GetSignalingSettings<WebSocketSignalingSettings>();
+            Assert.That(settings.urlSignaling, Is.EqualTo(url));
+            Assert.That(settings.iceServers, Is.EquivalentTo(iceServers));
         }
     }
 }
--- com.unity.renderstreaming/Tests/Runtime/RenderStreamingTest.cs
+++ com.unity.renderstreaming/Tests/Runtime/RenderStreamingTest.cs
@@ -1,10 +1,80 @@
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Unity.RenderStreaming.RuntimeTest
 {
-    class RenderStreamingTest
+    class RenderStreamingTest : IPrebuildSetup, IPostBuildCleanup
     {
+        private RenderStreamingSettings temp;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            temp = RenderStreaming.Settings;
+#if UNITY_EDITOR
+            // The test is performed with the default settings.
+            RenderStreaming.Settings =
+                AssetDatabase.LoadAssetAtPath<RenderStreamingSettings>(RenderStreaming.DefaultRenderStreamingSettingsPath);
+#endif
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (temp != null)
+            {
+                RenderStreaming.Settings = temp;
+            }
+        }
+
+        void IPrebuildSetup.Setup()
+        {
+#if UNITY_EDITOR
+            var defaultSettings = RenderStreaming.Settings;
+            RenderStreaming.Settings =
+                AssetDatabase.LoadAssetAtPath<RenderStreamingSettings>(RenderStreaming.DefaultRenderStreamingSettingsPath);
+            EditorBuildSettings.AddConfigObject(RenderStreaming.EditorBuildSettingsConfigKey, defaultSettings, true);
+#endif
+        }
+
+        void IPostBuildCleanup.Cleanup()
+        {
+#if UNITY_EDITOR
+            if (EditorBuildSettings.TryGetConfigObject(RenderStreaming.EditorBuildSettingsConfigKey, out RenderStreamingSettings settingsAsset))
+            {
+                RenderStreaming.Settings = settingsAsset;
+            }
+            else
+            {
+                RenderStreaming.Settings =
+                    AssetDatabase.LoadAssetAtPath<RenderStreamingSettings>(RenderStreaming.DefaultRenderStreamingSettingsPath);
+            }
+#endif
+        }
+
+        [Test]
+        public void LoadDefaultSettings()
+        {
+            RenderStreamingSettings defaultSettings = null;
+#if UNITY_EDITOR
+                defaultSettings =
+                    AssetDatabase.LoadAssetAtPath<RenderStreamingSettings>(RenderStreaming.DefaultRenderStreamingSettingsPath);
+#else
+                defaultSettings = Resources.FindObjectsOfTypeAll<RenderStreamingSettings>().FirstOrDefault() ??
+                                           ScriptableObject.CreateInstance<RenderStreamingSettings>();
+#endif
+            Assert.That(RenderStreaming.Settings.automaticStreaming, Is.EqualTo(defaultSettings.automaticStreaming));
+            Assert.That(RenderStreaming.Settings.signalingSettings.signalingClass, Is.EqualTo(defaultSettings.signalingSettings.signalingClass));
+            Assert.That(RenderStreaming.Settings.signalingSettings.urlSignaling, Is.EqualTo(defaultSettings.signalingSettings.urlSignaling));
+            Assert.That(RenderStreaming.Settings.signalingSettings.iceServers, Is.EquivalentTo(defaultSettings.signalingSettings.iceServers));
+        }
+
         [Test]
         public void Settings()
         {
