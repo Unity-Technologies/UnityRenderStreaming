414ecb28327e14a4b91c81a1717dcb82d88e1d96
kazuki <karasusan@gmail.com>
2023-02-14 17:32:57 +0900

    fix commandline argument parser
    (cherry picked from commit 0f4db549d72050cdf8a32dfb295d78875c0e92c9)

--- com.unity.renderstreaming/Runtime/Scripts/Signaling/HttpSignalingSettings.cs
+++ com.unity.renderstreaming/Runtime/Scripts/Signaling/HttpSignalingSettings.cs
@@ -105,7 +105,22 @@ namespace Unity.RenderStreaming
             var urls = CommandLineParser.IceServerUrls != null
                 ? CommandLineParser.IceServerUrls.Value
                 : null;
-            m_iceServers[0] = m_iceServers[0].Clone(username: username, credential: credential, credentialType: credentialType, urls: urls);
+
+            if (m_iceServers.Length > 0)
+                m_iceServers[0] = m_iceServers[0].Clone(
+                    username: username,
+                    credential: credential,
+                    credentialType: credentialType,
+                    urls: urls);
+            else
+                m_iceServers = new IceServer[]
+                    {
+                    new IceServer(
+                        username: username,
+                        credential: credential,
+                        credentialType: credentialType.Value,
+                        urls: urls)
+                    };
 
             if (CommandLineParser.PollingInterval.Value != null)
                 m_interval = CommandLineParser.PollingInterval.Value.Value;
