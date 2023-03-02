1e9f154c7fc263ea5e30e2765a8673725ec0c2f4
Aurelien Carrere <aurelien.carrere@unity3d.com>
2023-03-02 12:27:22 -0500

    Adding CORS support to streaming server

--- WebApp/src/server.ts
+++ WebApp/src/server.ts
@@ -7,6 +7,8 @@ import { log, LogLevel } from './log';
 import Options from './class/options';
 import { reset as resetHandler }from './class/httphandler';
 
+const cors = require('cors');
+
 export const createServer = (config: Options): express.Application => {
   const app: express.Application = express();
   resetHandler(config.mode);
@@ -15,6 +17,7 @@ export const createServer = (config: Options): express.Application => {
     app.use(morgan(config.logging));
   }
   // const signal = require('./signaling');
+  app.use(cors({origin: '*'}));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.get('/config', (req, res) => res.json({ useWebSocket: config.type == 'websocket', startupMode: config.mode, logging: config.logging }));
