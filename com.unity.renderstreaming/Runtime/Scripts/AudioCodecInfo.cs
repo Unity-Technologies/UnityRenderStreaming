aee157eed6f908cf7a8d01dd95c7f2d99b9a9901
Toru Nayuki <toru@unity3d.com>
2024-12-03 14:40:44 +0900

    doc: improve the API documentation of AudioCodecInfo

--- com.unity.renderstreaming/Runtime/Scripts/AudioCodecInfo.cs
+++ com.unity.renderstreaming/Runtime/Scripts/AudioCodecInfo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Unity.RenderStreaming
 {
     /// <summary>
-    /// 
+    /// Represents information about an audio codec, including its MIME type, SDP format parameters, channel count, and sample rate.
     /// </summary>
     [Serializable]
     public class AudioCodecInfo : IEquatable<AudioCodecInfo>
@@ -20,27 +20,27 @@ namespace Unity.RenderStreaming
         private int m_SampleRate;
 
         /// <summary>
-        /// 
+        /// Gets the name of the audio codec.
         /// </summary>
         public string name { get { return m_MimeType.GetCodecName(); } }
 
         /// <summary>
-        /// 
+        /// Gets the MIME type of the audio codec.
         /// </summary>
         public string mimeType { get { return m_MimeType; } }
 
         /// <summary>
-        /// 
+        /// Gets the number of audio channels.
         /// </summary>
         public int channelCount { get { return m_ChannelCount; } }
 
         /// <summary>
-        /// 
+        /// Gets the sample rate of the audio.
         /// </summary>
         public int sampleRate { get { return m_SampleRate; } }
 
         /// <summary>
-        /// 
+        /// Gets the SDP format parameters line.
         /// </summary>
         public string sdpFmtpLine { get { return m_SdpFmtpLine; } }
 
@@ -50,10 +50,17 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Determines whether the specified <see cref="AudioCodecInfo"/> is equal to the current <see cref="AudioCodecInfo"/>.
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// Debug.Log(audioStreamReceiver1.codec.Equals(audioStreamReceiver2.codec));
+        /// ]]>
+        ///</code>
+        /// </example>
+        /// <param name="other">The <see cref="AudioCodecInfo"/> to compare with the current <see cref="AudioCodecInfo"/>.</param>
+        /// <returns>true if the specified <see cref="AudioCodecInfo"/> is equal to the current <see cref="AudioCodecInfo"/>; otherwise, false.</returns>
         public bool Equals(AudioCodecInfo other)
         {
             if (other == null)
@@ -65,30 +72,30 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Determines whether the specified object is equal to the current <see cref="AudioCodecInfo"/>.
         /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
+        /// <param name="obj">The object to compare with the current <see cref="AudioCodecInfo"/>.</param>
+        /// <returns>true if the specified object is equal to the current <see cref="AudioCodecInfo"/>; otherwise, false.</returns>
         public override bool Equals(object obj)
         {
             return obj is AudioCodecInfo ? Equals((AudioCodecInfo)obj) : base.Equals(obj);
         }
 
         /// <summary>
-        /// 
+        /// Returns a hash code for the <see cref="AudioCodecInfo"/>.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A hash code for the current <see cref="AudioCodecInfo"/>.</returns>
         public override int GetHashCode()
         {
             return new { mimeType, sdpFmtpLine, channelCount, sampleRate }.GetHashCode();
         }
 
         /// <summary>
-        /// 
+        /// Determines whether two specified instances of <see cref="AudioCodecInfo"/> are equal.
         /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
+        /// <param name="left">The first <see cref="AudioCodecInfo"/> to compare.</param>
+        /// <param name="right">The second <see cref="AudioCodecInfo"/> to compare.</param>
+        /// <returns>true if the two <see cref="AudioCodecInfo"/> instances are equal; otherwise, false.</returns>
         public static bool operator ==(AudioCodecInfo left, AudioCodecInfo right)
         {
             if (ReferenceEquals(left, null))
@@ -102,11 +109,11 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Determines whether two specified instances of <see cref="AudioCodecInfo"/> are not equal.
         /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
+        /// <param name="left">The first <see cref="AudioCodecInfo"/> to compare.</param>
+        /// <param name="right">The second <see cref="AudioCodecInfo"/> to compare.</param>
+        /// <returns>true if the two <see cref="AudioCodecInfo"/> instances are not equal; otherwise, false.</returns>
         public static bool operator !=(AudioCodecInfo left, AudioCodecInfo right)
         {
             return !(left == right);
