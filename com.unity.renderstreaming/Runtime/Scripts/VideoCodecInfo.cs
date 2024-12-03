ca1643f2ec1f178989e8f19878febdebe7420656
Toru Nayuki <toru@unity3d.com>
2024-12-03 14:13:31 +0900

    doc: improve the API documentation of VideoCodecInfo

--- com.unity.renderstreaming/Runtime/Scripts/VideoCodecInfo.cs
+++ com.unity.renderstreaming/Runtime/Scripts/VideoCodecInfo.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace Unity.RenderStreaming
 {
     /// <summary>
-    /// 
+    /// Represents information about a video codec, including its MIME type, SDP format parameters.
     /// </summary>
     [Serializable]
     public class VideoCodecInfo : IEquatable<VideoCodecInfo>
@@ -21,30 +21,37 @@ namespace Unity.RenderStreaming
         readonly Dictionary<string, string> m_parameters = new Dictionary<string, string>();
 
         /// <summary>
-        /// 
+        /// Gets the name of the video codec.
         /// </summary>
         public string name { get { return m_MimeType.GetCodecName(); } }
 
         /// <summary>
-        /// 
+        /// Gets the MIME type of the video codec.
         /// </summary>
         public string mimeType { get { return m_MimeType; } }
 
         /// <summary>
-        /// 
+        /// Gets the codec implementation name.
         /// </summary>
         public string codecImplementation { get { return parameters[KeyCodecImplementation]; } }
 
         /// <summary>
-        /// 
+        /// Gets the SDP format parameters line.
         /// </summary>
         public string sdpFmtpLine { get { return m_SdpFmtpLine; } }
 
         /// <summary>
-        /// 
+        /// Determines whether the specified <see cref="VideoCodecInfo"/> is equal to the current <see cref="VideoCodecInfo"/>.
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        /// Debug.Log(videoStreamReceiver1.videoCodec.Equals(videoStreamReceiver2.videoCodec));
+        /// ]]>
+        ///</code>
+        /// </example>
+        /// <param name="other">The <see cref="VideoCodecInfo"/> to compare with the current <see cref="VideoCodecInfo"/>.</param>
+        /// <returns>True if the specified <see cref="VideoCodecInfo"/> is equal to the current <see cref="VideoCodecInfo"/>; otherwise, false.</returns>
         public bool Equals(VideoCodecInfo other)
         {
             if (other == null)
@@ -54,30 +61,31 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Determines whether the specified object is equal to the current <see cref="VideoCodecInfo"/>.
         /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
+        /// <param name="obj">The object to compare with the current <see cref="VideoCodecInfo"/>.</param>
+        /// <returns>True if the specified object is equal to the current <see cref="VideoCodecInfo"/>; otherwise, false.</returns>
         public override bool Equals(object obj)
         {
             return obj is VideoCodecInfo ? Equals((VideoCodecInfo)obj) : base.Equals(obj);
         }
 
         /// <summary>
-        /// 
+        /// Returns a hash code for the <see cref="VideoCodecInfo"/>.
+        /// The hash code is based on the MIME type and SDP format parameters line properties.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A hash code for the current <see cref="VideoCodecInfo"/>.</returns>
         public override int GetHashCode()
         {
             return new { mimeType, sdpFmtpLine }.GetHashCode();
         }
 
         /// <summary>
-        /// 
+        /// Determines whether two specified instances of <see cref="VideoCodecInfo"/> are equal.
         /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
+        /// <param name="left">The first <see cref="VideoCodecInfo"/> to compare.</param>
+        /// <param name="right">The second <see cref="VideoCodecInfo"/> to compare.</param>
+        /// <returns>True if the two <see cref="VideoCodecInfo"/> instances are equal; otherwise, false.</returns>
         public static bool operator ==(VideoCodecInfo left, VideoCodecInfo right)
         {
             if (ReferenceEquals(left, null))
@@ -91,11 +99,11 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Determines whether two specified instances of <see cref="VideoCodecInfo"/> are not equal.
         /// </summary>
-        /// <param name="left"></param>
-        /// <param name="right"></param>
-        /// <returns></returns>
+        /// <param name="left">The first <see cref="VideoCodecInfo"/> to compare.</param>
+        /// <param name="right">The second <see cref="VideoCodecInfo"/> to compare.</param>
+        /// <returns>True if the two <see cref="VideoCodecInfo"/> instances are not equal; otherwise, false.</returns>
         public static bool operator !=(VideoCodecInfo left, VideoCodecInfo right)
         {
             return !(left == right);
@@ -158,37 +166,37 @@ namespace Unity.RenderStreaming
     }
 
     /// <summary>
-    /// 
+    /// Represents the profiles for the VP9 video codec.
     /// </summary>
     public enum VP9Profile
     {
         /// <summary>
-        /// 
+        /// Profile 0.
         /// </summary>
         Profile0 = 0,
         /// <summary>
-        /// 
+        /// Profile 1.
         /// </summary>
         Profile1 = 1,
         /// <summary>
-        /// 
+        /// Profile 2.
         /// </summary>
         Profile2 = 2,
         /// <summary>
-        /// 
+        /// Profile 3.
         /// </summary>
         Profile3 = 3,
     }
 
     /// <summary>
-    /// 
+    /// Represents information about the VP9 video codec.
     /// </summary>
     public class VP9CodecInfo : VideoCodecInfo
     {
         const string KeyProfileId = "profile-id";
 
         /// <summary>
-        /// 
+        /// Gets the profile of the VP9 video codec.
         /// </summary>
         public VP9Profile? profile
         {
@@ -208,7 +216,7 @@ namespace Unity.RenderStreaming
     }
 
     /// <summary>
-    /// 
+    /// Represents the profiles for the H264 video codec.
     /// </summary>
     public enum H264Profile
     {
@@ -235,14 +243,14 @@ namespace Unity.RenderStreaming
     }
 
     /// <summary>
-    /// 
+    /// Represents information about the H264 video codec.
     /// </summary>
     public class H264CodecInfo : VideoCodecInfo
     {
         const string KeyProfileLevelId = "profile-level-id";
 
         /// <summary>
-        /// 
+        /// Gets the profile of the H264 video codec.
         /// </summary>
         public H264Profile profile
         {
@@ -250,7 +258,7 @@ namespace Unity.RenderStreaming
         }
 
         /// <summary>
-        /// 
+        /// Gets the level of the H264 video codec.
         /// </summary>
         public int level { get { return Convert.ToInt32(parameters[KeyProfileLevelId], 16) & 0xFF; } }
 
@@ -260,33 +268,33 @@ namespace Unity.RenderStreaming
     }
 
     /// <summary>
-    /// 
+    /// Represents the profiles for the AV1 video codec.
     /// </summary>
     public enum AV1Profile
     {
         /// <summary>
-        /// 
+        /// Profile 0.
         /// </summary>
         Profile0 = 0,
         /// <summary>
-        /// 
+        /// Profile 1.
         /// </summary>
         Profile1 = 1,
         /// <summary>
-        /// 
+        /// Profile 2.
         /// </summary>
         Profile2 = 2,
     }
 
     /// <summary>
-    /// 
+    /// Represents information about the AV1 video codec.
     /// </summary>
     public class AV1CodecInfo : VideoCodecInfo
     {
         const string KeyProfile = "profile";
 
         /// <summary>
-        /// 
+        /// Gets the profile of the AV1 video codec.
         /// </summary>
         public AV1Profile profile
         {
